57dcf045fc229f5561cb70751b8cd47faa4368f4
Orlando Chávez <60703406+oorlandochavez@users.noreply.github.com>
2020-02-27 15:29:03 -0600

    actualizacion init instructor

--- resources/views/layouts/pages/initinstructor.blade.php
+++ resources/views/layouts/pages/initinstructor.blade.php
@@ -22,7 +22,6 @@
     </style>
     <div class="container g-pt-50">
         <div class="row">
-
             <div class="col-lg-12 margin-tb">
                 <div class="pull-left">
                     <h2>Registro de Instructrores</h2>
@@ -38,50 +37,29 @@
             <caption>Catalogo de Instructrores</caption>
             <thead>
                 <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
-                    <th width="160px">Action</th>
+                    <th scope="col">Clave Instructor</th>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">telefono</th>
+                    <th scope="col">Correo</th>
+                    <th width="160px">Acciones</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    <td>
-                        <a class="btn btn-info" href="{{route('instructor-ver')}}">Mostrar</a>
-                        {!! Form::open(['method' => 'DELETE','route' => ['usuarios'],'style'=>'display:inline']) !!}
-                        {!! Form::submit('Borrar', ['class' => 'btn btn-danger']) !!}
-                        {!! Form::close() !!}
-                    </td>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    <td>
-                        <a class="btn btn-info" href="{{route('instructor-ver')}}">Mostrar</a>
-                        {!! Form::open(['method' => 'DELETE','route' => ['usuarios'],'style'=>'display:inline']) !!}
-                        {!! Form::submit('Borrar', ['class' => 'btn btn-danger']) !!}
-                        {!! Form::close() !!}
-                    </td>
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
-                    <td>
-                        <a class="btn btn-info" href="{{route('instructor-ver')}}">Mostrar</a>
-                        {!! Form::open(['method' => 'DELETE','route' => ['usuarios'],'style'=>'display:inline']) !!}
-                        {!! Form::submit('Borrar', ['class' => 'btn btn-danger']) !!}
-                        {!! Form::close() !!}
-                    </td>
-                </tr>
+                @foreach ($data as $itemData)
+                    <tr>
+                    <th scope="row">{{$itemData->numero_control}}</th>
+                        <td>{{$itemData->nombre}}</td>
+                        <td>{{$itemData->telefono}}</td>
+                        <td>{{$itemData->correo}}</td>
+                        <td>
+                            <a class="btn btn-info" href="{{route('instructor-ver')}}">Mostrar</a>
+                            {!! Form::open(['method' => 'DELETE','route' => ['usuarios'],'style'=>'display:inline']) !!}
+                            {!! Form::submit('Borrar', ['class' => 'btn btn-danger']) !!}
+                            {!! Form::close() !!}
+                        </td>
+                    </tr>
+                @endforeach
+                {{ $data->links() }}
             </tbody>
         </table>
     </div>
