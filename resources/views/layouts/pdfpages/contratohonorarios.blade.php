a11c1f79bfbe860de496e9842e26746f94be5edf
tejeda22 <tejeda2219@gmail.com>
2020-03-02 14:04:57 -0600

    adrian2

--- resources/views/layouts/pdfpages/contratohonorarios.blade.php
+++ resources/views/layouts/pdfpages/contratohonorarios.blade.php
@@ -69,15 +69,92 @@
               <dd>II.4 Goza de plena capacidad jurídica y facultades que le otorga la ley, para contratar y obligarse, así como también con los estudios, conocimientos y la experiencia necesaria en la materia de ___materia_especializacion____________________________ y conoce plenamente las necesidades de los servicios objeto del presente contrato, así como que ha considerado todos los factores que intervienen para desarrollar eficazmente las actividades que desempeñará. </dd>
               <dd>II.5 Es conforme de que “EL ICATECH”, le retenga los impuestos a que haya lugar por concepto de la Prestación de Servicios Profesionales por Honorarios y/o por Honorarios Asimilables a Salarios. </dd>
               <dd>II.6 Bajo protesta de decir verdad, no se encuentra inhabilitado por autoridad competente alguna, así como a la suscripción del presente documento no ha sido parte en juicios del orden civil, mercantil o laboral en contra de “EL ICATECH” o de alguna otra institución pública o privada; y que no se encuentra en algún otro supuesto o situación que pudiera generar conflicto de intereses para prestar los servicios profesionales objeto del presente contrato. </dd>
-              <dd>II.7 Para los efectos del presente contrato, señala como su domicilio legal el ubicado en _________________ _________________domicilio_________________. 
-              </dl>
+              <dd>II.7 Para los efectos del presente contrato, señala como su domicilio legal el ubicado en _________________ _________________domicilio_________________.</dl>
           </div>
           <div align="justify">Con base en las declaraciones antes expuestas, declaran las partes que es su voluntad celebrar el presente contrato, sujetándose a las siguientes:</div>
           <div align="center"> Clausulas </div>
-          <div align="justify"><dl><dt>PRIMERA.- OBJETO DEL CONTRATO. En los términos y condiciones del presente contrato “EL PRESTADOR DE SERVICIOS” se obliga a prestar sus servicios profesionales por Honorarios y/o Honorarios Asimilables a Salarios a “EL ICATECH”, consistentes en otorgar el curso que se detalla en el Anexo 4, formando parte integrante del presente contrato como si a la letra se insertase, donde se establece bajo que especialidad, periodo de impartición, horario, días, horas que se cubrirán y en el lugar (establecimiento) que será impartido el curso.</dt>
+          <div align="justify"><dd><b> PRIMERA.- OBJETO DEL CONTRATO.</b>En los términos y condiciones del presente contrato “EL PRESTADOR DE SERVICIOS” se obliga a prestar sus servicios profesionales por Honorarios y/o Honorarios Asimilables a Salarios a “EL ICATECH”, consistentes en otorgar el curso que se detalla en el Anexo 4, formando parte integrante del presente contrato como si a la letra se insertase, donde se establece bajo que especialidad, periodo de impartición, horario, días, horas que se cubrirán y en el lugar (establecimiento) que será impartido el curso.</dd>
+            <dd>SEGUNDA.- MONTO DE LOS HONORARIOS. El monto total de los honorarios que “EL ICATECH”, pagará a “EL PRESTADOR DE SERVICIOS” será por la cantidad de $__,_____.__ (_____cantidad_letra______________ pesos ___/100 M.N.), más el 16% (dieciséis por ciento), del Impuesto al Valor Agregado, menos las retenciones que “EL ICATECH” le retendrá como pago provisional por concepto del Impuesto Sobre la Renta e Impuesto al Valor Agregado, enterando a las autoridades hacendarias las retenciones correspondientes.</dd>
+            <dd>De tratarse el pago por concepto de Honorarios Asimilables a Salarios, el monto que “EL ICATECH”, pagará a “EL PRESTADOR DE SERVICIOS” será por la cantidad de $__,_____.__ (__________contidad_letra2______ _________________________ pesos ___/100 M.N.), menos la retención que “EL ICATECH” le retendrá como pago provisional por concepto del Impuesto Sobre la Renta, enterando a las autoridades hacendarias la retención correspondiente.</dd>
+            <dd>“EL PRESTADOR DE SERVICIOS” recibirá el monto resultante señalado en el párrafo primero de esta cláusula si el pago se trata por concepto de Honorarios, de tratarse el pago por concepto de Honorarios Asimilables a Salarios, recibirá el monto resultante señalado en el párrafo segundo de esta misma cláusula, en ambos casos él pago se realizará en una sola exhibición, por medio de transferencia electrónica interbancaria a la cuenta que señala y se agrega al presente contrato como Anexo 5, formando parte integrante del presente instrumento como si a la letra se insertase, después de haber concluido el curso, contra la entrega del recibo de honorarios y/o factura correspondiente, y en su caso del recibo de nómina (las cuales se agregaran al presente Instrumento como Anexo 6), mismo que deberá cubrir los requisitos fiscales estipulados por la Secretaría de Hacienda y Crédito Público; por lo que “EL PRESTADOR DE SERVICIOS” no podrá exigir retribución alguna por ningún otro concepto.</dd>
+            TERCERA.- DE LA OBLIGACIÓN DE “EL PRESTADOR DE SERVICIOS”. “EL PRESTADOR DE SERVICIOS” se obliga a desempeñar las obligaciones que contrae en este acto y con todo el sentido ético y profesional que requiere “EL ICATECH” y de acuerdo con las políticas y reglamentos del mismo. “EL PRESTADOR DE SERVICIOS” declara conocer y se obliga a:
+            <Ol type = "I">
+              <li> Diseñar, preparar y dictar los cursos a su cargo con toda la diligencia y esmero que exige la calidad de “EL ICATECH”.</ li>
+              <li> Asistir con toda puntualidad a sus cursos y aprovechar íntegramente el tiempo necesario para el mejor desarrollo de los mismos. </ li>
+              <li> Generar un reporte final del curso impartido o de cualquier incidente o problema que surgió en el desarrollo del mismo.</ li>
+              <li> Cumplir con los procedimientos del control escolar de alumnos que implemente “EL ICATECH”. </ li>
+              <li> Respetar las normas de conducta que establece “EL ICATECH”. </ li> 
+            </ Ol>
           </div>
-      </div>
-   </div>
+          <div align="justify"><dd>CUARTA.- SECRETO PROFESIONAL. “EL PRESTADOR DE SERVICIOS” conviene no divulgar por medio de publicaciones, informes, conferencias o en cualquier otra forma, los datos y resultados obtenidos de los trabajos de este contrato, sin la autorización expresa de “EL ICATECH”, pues dichos datos y resultados son considerados confidenciales. Esta obligación subsistirá, aún después de haber terminado la vigencia de este contrato</dd>
+          <dd>QUINTA.- VIGENCIA. La vigencia del presente contrato será conforme a la duración del Curso objeto del presente Instrumento, detallados en la CLÁUSULA PRIMERA; el cual será forzoso para “EL PRESTADOR DE SERVICIOS” y voluntario para “EL ICATECH” mismo que podrá darlo por terminado anticipadamente en cualquier tiempo, siempre y cuando existan motivos o razones de interés general, incumpla cualquiera de las obligaciones adquiridas con la formalización del presente instrumento o incurra en alguna de las causales previstas en la Cláusula Octava, mediante notificación por escrito a “EL PRESTADOR DE SERVICIOS”; en todo caso “EL ICATECH” deberá cubrir los honorarios únicamente en cuanto a los días de servicio prestados.</dd>
+          <dd>Concluido el término del presente contrato no podrá haber prórroga automática por el simple transcurso del tiempo y terminará sin necesidad de darse aviso entre las partes. Si terminada la vigencia de este contrato, “EL ICATECH” tuviere necesidad de seguir utilizando los servicios de “EL PRESTADOR DE SERVICIOS”, se requerirá la celebración de un nuevo contrato, sin que éste pueda ser computado con el anterior. </dd>
+          <dd>SEXTA.- SEGUIMIENTO. “EL ICATECH” a través de los representantes que al efecto designe, tendrá en todo tiempo el derecho de supervisar el estricto cumplimiento de este contrato, por lo que podrá revisar e inspeccionar las actividades que desempeñe “EL PRESTADOR DE SERVICIOS”. </dd>
+          <dd>SÉPTIMA.- PROPIEDAD DE RESULTADOS Y DERECHOS DE AUTOR. Los documentos, estudios y demás materiales que se generen en la ejecución o como consecuencia de este contrato, pasarán a ser propiedad de “EL ICATECH”, quedando obligado “EL PRESTADOR DE SERVICIOS” a entregarlos al término del presente instrumento</dd>
+          <dd>“EL PRESTADOR DE SERVICIOS” se obliga con “EL ICATECH” a responder ilimitadamente de los daños o perjuicios que pudiera causar a “EL ICATECH” o a terceros, si con motivo de la prestación de los servicios contratados viola derechos de autor, de patentes y/o marcas u otro derecho reservado, por lo anterior “EL PRESTADOR DE SERVICIOS” manifiesta en este acto bajo protesta de decir verdad, no encontrarse en ninguno de los supuestos de infracción a la Ley Federal de Derechos de Autor ni a la Ley de Propiedad Industrial. </dd>
+          <dd>En caso de que sobreviniera alguna reclamación en contra de “EL ICATECH” por cualquiera de las causas antes mencionadas, la única obligación de éste será dar aviso a “EL PRESTADOR DE SERVICIOS” en el domicilio previsto en este instrumento para que ponga a salvo a “EL ICATECH” de cualquier controversia. </dd>
+          <dd>OCTAVA.- RESCISIÓN. “EL ICATECH” podrá rescindir el presente contrato sin responsabilidad alguna, sin necesidad de declaración judicial, bastando para ello una notificación por escrito cuando concurran causas de interés general, cuando “EL PRESTADOR DE SERVICIOS” incumpla algunas de las obligaciones del presente contrato y demás disposiciones contenidas en las leyes que le sean aplicables y cuando a juicio de “EL ICATECH” incurra en las siguientes causales: </dd>
+         </div>
+            <ol type = "I">
+              <li>Negligencia o impericia. </li>
+              <li>Falta de probidad u honradez.</li>
+              <li>Por prestar los servicios de forma ineficiente e inoportuna. </li>
+              <li>Por no apegarse a lo estipulado en el presente contrato.</li>
+              <li>Por no observar la discreción debida respecto a la información a la que tenga acceso como consecuencia de la información de los servicios encomendados.</li>
+              <li>Por suspender injustificadamente la prestación de los servicios o por negarse a corregir lo rechazado por “EL ICATECH”. </li>
+              <li>Por negarse a informar a “EL ICATECH” sobre los resultados de la prestación del servicio encomendado. </li>
+              <li>Por impedir el desempeño normal de las labores durante la prestación de los servicios. </li>
+              <li>Si se comprueba que la protesta a que se refiere la Declaración II.2 de “EL PRESTADOR DE SERVICIOS” se realizó con falsedad. </li>
+              <li>Por no otorgar los cursos en el tiempo establecido (horas del curso). </li>  
+            </ol>
+          <div align="justify"><dd>Asimismo, en caso de tener evidencias de que el curso no fue impartido, se procederá a dar por rescindido el contrato, y se actuará conforme a lo dispuesto por la Ley de Responsabilidad Administrativa para el Estado de Chiapas.  </dd>
+            <dd>“EL PRESTADOR DE SERVICIOS” podrá dar por rescindido de forma anticipada el presente contrato, previo aviso que realice por escrito con un mínimo de 10 días hábiles a “EL ICATECH”. </dd>
+            <dd>“EL ICATECH” se reservará el derecho de aceptar la terminación anticipada del contrato, sin que ello implique la renuncia a deducir las acciones legales que en su caso procedan.</dd>
+            <dd>NOVENA.- CESIÓN. “EL PRESTADOR DE SERVICIOS” no podrá en ningún caso ceder total o parcialmente a terceros llámese persona física o persona moral, los derechos y obligaciones derivadas del presente contrato. </dd>
+            <dd>DÉCIMA.- RELACIONES PROFESIONALES. “EL ICATECH” no adquiere ni reconoce obligación alguna de carácter laboral a favor de “EL PRESTADOR DE SERVICIOS”, en virtud de no ser aplicables a la relación contractual que consta en este instrumento, los artículos 1º y 8º de la Ley Federal del Trabajo y 123 apartado “A” de la Constitución Política de los Estados Unidos Mexicanos, por lo que “EL PRESTADOR DE SERVICIOS”, no será considerado como trabajador para los efectos legales y en particular para obtener las prestaciones establecidas en su artículo 5 A, fracciones V, VI y VII de la Ley del Instituto Mexicano del Seguro Social. </dd>
+            <dd>“EL PRESTADOR DE SERVICIOS” acepta ser el único responsable del cumplimiento de las obligaciones derivadas de las relaciones laborales, fiscales, contractuales o de cualquier otra índole, incluso las de seguridad social e INFONAVIT que pudieran derivarse de la prestación de los servicios aquí contratados, consecuentemente “EL PRESTADOR DE SERVICIOS” libera de toda responsabilidad a “EL ICATECH” de las obligaciones que pudieran presentarse por éstos conceptos. </dd>
+            <dd>“EL PRESTADOR DE SERVICIOS” se obliga a prestar sus servicios en el domicilio previamente convenido por las partes. </dd>
+            <dd>DÉCIMA PRIMERA.- “EL PRESTADOR DE SERVICIOS” reconoce que la relación existente con “EL ICATECH” es de naturaleza civil, por tanto, en ningún momento podrá ser considerado como trabajador de “EL ICATECH” con motivo del cumplimiento de las obligaciones derivadas del presente instrumento, por lo cual libera a éste de cualquier responsabilidad laboral. </dd>
+            <dd>DÉCIMA SEGUNDA.- RECONOCIMIENTO CONTRACTUAL. El presente contrato se rige por lo dispuesto en el Título Décimo del Contrato de Prestación de Servicios, Capitulo I, del Código Civil del Estado de Chiapas, por lo que no existe relación de dependencia ni de subordinación entre “EL ICATECH” y “EL PRESTADOR DE SERVICIOS”, ni podrán tenerse como tales los necesarios nexos de coordinación entre uno y otro. </dd>
+            <dd>El presente contrato constituye el acuerdo de voluntades entre las partes, en relación con el objeto del mismo y deja sin efecto cualquier otra negociación o comunicación entre éstas, ya sea oral o escrita con anterioridad a la fecha de su firma. </dd>
+            <dd>DÉCIMA TERCERA.- Manifiestan ambas partes bajo protesta de decir verdad que en el presente contrato no ha mediado dolo, error, mala fe, engaño, violencia, intimidación, ni cualquiera otra causa que pudiera invalidar el contenido y fuerza legal del mismo. </dd>
+            <dd>DÉCIMA CUARTA.- DOMICILIOS. Para los efectos del presente instrumento las partes señalan como sus domicilios legales los siguientes: </dd>
+            <dd>“EL ICATECH”: ubicado en la 14 poniente norte número 239, de la Colonia Moctezuma, con Código Postal 29030, en la Ciudad de Tuxtla Gutiérrez, Chiapas. </dd>
+            <dd>“EL PRESTADOR DE SERVICIOS”: _____________comentario_instructor_______________________
+              _________________________________________________________________________________________.</dd>
+            <dd>Mientras las partes no notifiquen por escrito el cambio de su domicilio, los emplazamientos y demás diligencias judiciales y extrajudiciales, se practicarán en el domicilio señalado en esta cláusula. </dd>
+            <dd>DÉCIMA QUINTA.- RESPONSABILIDAD DE “EL PRESTADOR DE SERVICIOS”. “EL PRESTADOR DE SERVICIOS” será el responsable de la ejecución de los trabajos y deberá sujetarse en la realización de éstos, a todos aquellos reglamentos administrativos y manuales que las autoridades competentes hayan emitido, así como a las disposiciones establecidas al efecto por “EL ICATECH”. </dd>
+            <dd>DÉCIMA SEXTA.- JURISDICCIÓN. Para la interpretación y cumplimiento del presente contrato, así como para todo aquello que no esté expresamente estipulado en el mismo, las partes se someterán a la jurisdicción y competencia de los tribunales del fuero común de la ciudad de Tuxtla Gutiérrez, Chiapas, renunciando al fuero que pudiera corresponderles por razón de su domicilio presente o futuro.  </dd>
+            <dd>Leído que fue el presente contrato a las partes que en él intervienen y una vez enterados de su contenido y alcance legales, son conformes con los términos del mismo y para constancia lo firman y ratifican ante la presencia de los testigos que al final suscriben; en el municipio de ______________________, Chiapas; el día ______ de _________ del año 2020, en original y dos tantos. </dd>
+            </div>
+          
+          <table class="table">
+            <caption>List of users</caption>
+            <thead>
+              <tr>
+                <th scope="col"></th>
+                <th scope="col">         "EL ICATECH"</th>
+                <th scope="col"></th>
+                <th scope="col">"EL PRESTADOR DE SERVICIOS"</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row"></th>
+                <td>_____________________________________________</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <th scope="row"></th>
+                <td>DIRECTOR (A) DE LA UNIDAD DE CAPACITACION</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+              </tr>
+            </tbody>
+          </table>
+          
+     </div>
  </body>
 </html>
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
