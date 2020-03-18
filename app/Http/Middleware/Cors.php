2fbe41796189b3956ae6792c348d77adf12ca273
danielmendez88 <daniboymendez@gmail.com>
2020-03-18 05:53:45 +0100

    modificaciones en la configuracion de los cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,9 +16,9 @@ class Cors
     public function handle($request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Origin', 'https://www.sivyc.icatech.gob.mx')
             ->header('content-type:', 'application/json; charset=utf-8')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE', 'OPTIONS')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
             ->header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-auth, Authorization, Access-Control-Allow-Origin');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,8 @@ use Illuminate\Http\Request;
 //     return $request->user();
 // });
 
-Route::middleware('cors')->group(function () {
-    Route::resource('cursos', 'ApiController\CursosController');
+Route::group(['middleware' => ['cors']], function(){
+	Route::resource('cursos', 'ApiController\CursosController');
     Route::resource('Instructores', 'ApiController\InstructoresController');
     Route::get('Instructoreshow/{nombre}/{apaterno}/{apmaterno}', 'ApiController\InstructoresController@show');
 });
