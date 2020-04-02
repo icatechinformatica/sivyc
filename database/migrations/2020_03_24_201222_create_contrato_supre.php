d9a473ffa33836b90c3c32641f01798b7d20529a
Orlando Chávez <60703406+oorlandochavez@users.noreply.github.com>
2020-04-02 10:11:42 -0600

    modificacion de realcion de supre_directorio

--- database/migrations/2020_03_24_201222_create_contrato_supre.php
+++ database/migrations/2020_03_24_201222_create_contrato_supre.php
@@ -15,6 +15,12 @@ class CreateContratoSupre extends Migration
     {
         Schema::create('contrato_supre', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->integer('supre_dest');
+            $table->integer('supre_rem');
+            $table->integer('supre_valida');
+            $table->integer('supre_elabora');
+            $table->integer('supre_ccp1');
+            $table->integer('supre_ccp2');
             $table->timestamps();
         });
     }
