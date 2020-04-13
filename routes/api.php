32143263611ff2b911613122775a8810cdeb6635
daniel mendez cruz <danielmendez.88@live.com>
2020-04-13 11:59:25 -0500

    modificacion routes api

--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,5 @@ Route::group(['middleware' => ['cors']], function(){
 	Route::resource('cursos', 'ApiController\CursosController');
     Route::resource('Instructores', 'ApiController\InstructoresController');
     Route::get('Instructoreshow/{nombre}/{apaterno}/{apmaterno}', 'ApiController\InstructoresController@show');
+    Route::resource('catalogo-cursos', 'ApiController\CatalogoCursoController');
 });
