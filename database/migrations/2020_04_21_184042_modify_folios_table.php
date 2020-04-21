42add1b62ad1900358c44ad763be76827c68dcf1
daniel mendez cruz <danielmendez.88@live.com>
2020-04-21 14:10:16 -0500

    modificacio tabla folios

---
+++ database/migrations/2020_04_21_184042_modify_folios_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ModifyFoliosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('folios', function (Blueprint $table) {
+            //
+            $table->decimal('iva', 8, 2)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('folios', function (Blueprint $table) {
+            //
+        });
+    }
+}
