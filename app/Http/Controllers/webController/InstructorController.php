cbb2bcc0173363e1f9d0869140e40e1652e31baf
daniel mendez cruz <danielmendez.88@live.com>
2020-05-27 20:50:57 -0500

    modificacion

--- app/Http/Controllers/webController/InstructorController.php
+++ app/Http/Controllers/webController/InstructorController.php
@@ -285,9 +285,9 @@ class InstructorController extends Controller
                         ->LEFTJOIN('especialidad_instructores','especialidad_instructores.perfilprof_id','=','instructor_perfil.id')
                         ->LEFTJOIN('especialidades','especialidades.id','=','especialidad_instructores.especialidad_id')
                         ->LEFTJOIN('criterio_pago','criterio_pago.id','=','especialidad_instructores.pago_id')
-                        ->FIRST();
+                        ->GET();
 
-        dd(count($validado));
+        dd($validado->exists());
 
         return view('layouts.pages.verinstructor', compact('datains','estado_civil','lista_civil','unidad','lista_unidad','perfil','validado'));
     }
