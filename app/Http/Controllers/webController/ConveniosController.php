25ffd66739bf02ec9b3fac9400110e9a58e6e46d
daniel mendez cruz <danielmendez.88@live.com>
2020-05-31 15:06:08 -0500

    modificaciones en convenios

--- app/Http/Controllers/webController/ConveniosController.php
+++ app/Http/Controllers/webController/ConveniosController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\File;
+use App\Models\Municipio;
 
 class ConveniosController extends Controller
 {
@@ -32,8 +33,10 @@ class ConveniosController extends Controller
      */
     public function create()
     {
+        $municipio = new Municipio();
+        $municipios = $municipio->all();
         // mostrar formulario de convenio
-        return view('layouts.pages.frmconvenio');
+        return view('layouts.pages.frmconvenio', compact('municipios'));
     }
 
     /**
@@ -49,7 +52,6 @@ class ConveniosController extends Controller
         $validador = Validator::make($request->all(), [
             'no_convenio' => 'required',
             'institucion' => 'required',
-            'tipo' => 'required',
             'telefono' => 'required',
             'sector' => 'required',
             'fecha_firma' => 'required',
@@ -60,7 +62,6 @@ class ConveniosController extends Controller
             'nombre_enlace' => 'required',
             'status' => 'required',
             'direccion' => 'required',
-            'archivo_convenio' => 'max:2048|mimes:pdf'
         ]);
 
         if ($validador->fails()) {
@@ -70,18 +71,18 @@ class ConveniosController extends Controller
         }
 
         $convenios = new Convenio;
-        $convenios['no_convenio'] = $request->input('no_convenio');
-        $convenios['institucion'] = $request->input('institucion');
-        $convenios['tipo_sector'] = $request->input('tipo');
-        $convenios['telefono'] = $request->input('telefono');
-        $convenios['fecha_firma'] = date('Y-m-d', strtotime($request->input('fecha_firma')));
-        $convenios['fecha_vigencia'] = date('Y-m-d H:i:s', strtotime($request->input('fecha_termino')));
-        $convenios['poblacion'] = $request->input('poblacion');
-        $convenios['municipio'] = $request->input('municipio');
-        $convenios['nombre_titular'] = $request->input('nombre_titular');
-        $convenios['nombre_enlace'] = $request->input('nombre_enlace');
-        $convenios['status'] = $request->input('status');
-        $convenios['direccion'] = $request->input('direccion');
+        $convenios['no_convenio'] = trim($request->input('no_convenio'));
+        $convenios['institucion'] = trim($request->input('institucion'));
+        $convenios['tipo_sector'] = $request->input('sector');
+        $convenios['telefono'] = trim($request->input('telefono'));
+        $convenios['fecha_firma'] = $convenios->getMyDateFormat($request->input('fecha_firma'));
+        $convenios['fecha_vigencia'] = $convenios->getMyDateFormat($request->input('fecha_termino'));
+        $convenios['poblacion'] = trim($request->input('poblacion'));
+        $convenios['municipio'] = trim($request->input('municipio'));
+        $convenios['nombre_titular'] = trim($request->input('nombre_titular'));
+        $convenios['nombre_enlace'] = trim($request->input('nombre_enlace'));
+        $convenios['status'] = trim($request->input('status'));
+        $convenios['direccion'] = trim($request->input('direccion'));
 
         $convenios->save();
 
@@ -91,18 +92,30 @@ class ConveniosController extends Controller
         $convenioId = $convenios->id;
 
         if ($request->hasFile('archivo_convenio')) {
-            # vamos a trabajar en el documento para guardarlo
-            $archivoConvenio = $request->file('archivo_convenio');
-            $extension_archivo = $archivoConvenio->getClientOriginalExtension(); # extension de archivo del cliente
-            $fileSize = $archivoConvenio->getClientSize(); # tamaño del archivo
-            # nuevo nombre del archivo
-            $fileName = "convenio".date('YmdHis')."_".$convenioId.".".$extension_archivo;
-            $request->file('archivo_convenio')->storeAs('/convenios/'.$convenioId, $fileName);
-            $docUrl = Storage::url('/convenios/'.$convenioId."/".$fileName);
-            // guardamos en la base de datos
-            $convenioUpdate = Convenio::find($convenioId);
-            $convenioUpdate->archivo_convenio = $docUrl;
-            $convenioUpdate->save();
+            #ANTES DE GUARDAR EL ARCHIVO SI ES QUE HAY UNO VALIDAMOS QUE CUMPLA CON EL REQUERIMIENTO
+
+            $validator = Validator::make($request->all(), [
+                'archivo_convenio' => 'max:2048|mimes:pdf',
+            ]);
+
+            if ($validator->fails()) {
+                # code...
+                return redirect('convenios/crear')
+                        ->withErrors($validator);
+            } else {
+                # vamos a trabajar en el documento para guardarlo
+                $archivoConvenio = $request->file('archivo_convenio');
+                $extension_archivo = $archivoConvenio->getClientOriginalExtension(); # extension de archivo del cliente
+                $fileSize = $archivoConvenio->getClientSize(); # tamaño del archivo
+                # nuevo nombre del archivo
+                $fileName = "convenio".date('YmdHis')."_".$convenioId.".".$extension_archivo;
+                $request->file('archivo_convenio')->storeAs('/convenios/'.$convenioId, $fileName);
+                $docUrl = Storage::url('/convenios/'.$convenioId."/".$fileName);
+                // guardamos en la base de datos
+                $convenioUpdate = Convenio::find($convenioId);
+                $convenioUpdate->archivo_convenio = $docUrl;
+                $convenioUpdate->save();
+            }
         }
 
         return redirect('/convenios/indice')->with('success', 'Convenio Agreado exitosamente!');
--- resources/views/layouts/pages/frmconvenio.blade.php
+++ resources/views/layouts/pages/frmconvenio.blade.php
@@ -18,89 +18,89 @@
     <form method="POST" action="{{ route('convenios.store') }}" id="conveniosFrm" enctype="multipart/form-data" autocomplete="off">
         @csrf
         <div style="text-align: center">
-            <label for="tituloagregar_convenio"><h1>Agregar Convenio</h1></label>
+            <label for="tituloagregar_convenio"><h1>NUEVO CONVENIO</h1></label>
          </div>
          <hr style="border-color:dimgray">
         <div class="form-row">
             <div class="form-group col-md-6">
-                <label for="no_convenio" class="control-label">N° Contrato</label>
+                <label for="no_convenio" class="control-label">N° CONVENIO</label>
                 <input type="text" class="form-control" id="no_convenio" name="no_convenio" placeholder="N° Convenio">
             </div>
-        </div>
-        <div class="form-row">
             <!-- Organismo -->
             <div class="form-group col-md-6">
-                <label for="institucion" class="control-label">Institución</label>
+                <label for="institucion" class="control-label">INSTITUCIÓN</label>
                 <input type="text" class="form-control" id="institucion" name="institucion" placeholder="Institución">
             </div>
             <!--Organismo Fin-->
-            <!-- Direccion -->
-            <div class="form-group col-md-6">
-                <label for="tipo" class="control-label">Tipo</label>
-                <input type="text" class="form-control" id="tipo" name="tipo" placeholder="Tipo">
-            </div>
-            <!-- Direccion FIN-->
         </div>
-
         <div class="form-row">
             <!-- Telefono -->
-            <div class="form-group col-md-6">
-                <label for="telefono" class="control-label">Telefono </label>
+            <div class="form-group col-md-4">
+                <label for="telefono" class="control-label">TELÉFONO</label>
                 <input type="text" class="form-control" onkeypress="return solonumeros(event)" id="telefono" name="telefono" placeholder="telefono">
             </div>
             <!--Telefono Fin-->
             <!-- Tipo de sector -->
-            <div class="form-group col-md-6">
-                <label for="sector">Tipo de Sector</label>
+            <div class="form-group col-md-4">
+                <label for="sector">SECTOR</label>
                 <select class="form-control" id="sector" name="sector">
-                    <option value="">----</option>
-                    <option value="publico">Publico</option>
-                    <option value="privado">Privado</option>
+                    <option value="">--SELECCIONAR--</option>
+                    <option value="PUBLICO">PÚBLICO</option>
+                    <option value="PRIVADO">PRIVADO</option>
                 </select>
             </div>
             <!-- Fin Sector-->
+            <div class="form-group col-md-4">
+                <label for="status">ESTATUS</label>
+                <select class="form-control" id="status" name="status">
+                    <option value="">--SELECCIONAR--</option>
+                    <option value="true">ACTIVO</option>
+                    <option value="false">TERMINADO</option>
+                </select>
+            </div>
         </div>
-
         <div class="form-row">
             <!-- fecha inicial -->
-            <div class="form-group col-md-6">
-                <label for="fecha_firma" class="control-label">Fecha de Firma </label>
+            <div class="form-group col-md-4">
+                <label for="fecha_firma" class="control-label">FECHA DE LA FIRMA</label>
                 <input type='text' id="fecha_firma" autocomplete="off" readonly="readonly" name="fecha_firma" class="form-control datepicker" />
             </div>
             <!--Fecha inicial END-->
             <!-- Fecha conclusion -->
-            <div class="form-group col-md-6">
-                <label for="fecha_termino" class="control-label">Fecha de Termino </label>
+            <div class="form-group col-md-4">
+                <label for="fecha_termino" class="control-label">FECHA DE TERMINO</label>
                 <input type='text' id="fecha_termino" autocomplete="off" readonly="readonly" name="fecha_termino" class="form-control datepicker" />
             </div>
             <!-- Fecha conclusion END-->
-        </div>
-
-        <div class="form-row">
             <!--poblacion-->
-            <div class="form-group col-md-6">
-                <label for="poblacion" class="control-label">Población </label>
+            <div class="form-group col-md-4">
+                <label for="poblacion" class="control-label">POBLACIÓN</label>
                 <input type='text' id="poblacion" name="poblacion" class="form-control" />
             </div>
             <!--poblacion END-->
-            <!--municipio-->
-            <div class="form-group col-md-6">
-                <label for="municipio" class="control-label">Municipio</label>
-                <input type='text' id="municipio" name="municipio" class="form-control" />
-            </div>
-            <!--municipio END-->
         </div>
 
         <div class="form-row">
+            <!--municipio-->
+            <div class="form-group col-md-4">
+                <label for="municipio" class="control-label">MUNICIPIO</label>
+                <select class="form-control" name="municipio" id="municipio">
+                    <option value="">--SELECCIONAR--</option>
+                    @foreach ($municipios as $itemMunicipio)
+                        <option value="{{$itemMunicipio->muni}}">{{$itemMunicipio->muni}}</option>
+                    @endforeach
+                </select>
+            </div>
+            <!--municipio END-->
             <!--nombre_titular-->
-            <div class="form-group col-md-6">
-                <label for="nombre_titular" class="control-label">Nombre del Titular </label>
+            <div class="form-group col-md-4">
+                <label for="nombre_titular" class="control-label">NOMBRE DEL TITULAR</label>
                 <input type='text' id="nombre_titular" name="nombre_titular" class="form-control" />
             </div>
             <!--nombre_titular END-->
             <!--nombre_enlace-->
-            <div class="form-group col-md-6">
-                <label for="nombre_enlace" class="control-label">Nombre Enlace </label>
+            <div class="form-group col-md-4">
+                <label for="nombre_enlace" class="control-label">NOMBRE DEL ENLACE</label>
                 <input type='text' id="nombre_enlace" name="nombre_enlace" class="form-control" />
             </div>
             <!--nombre_enlace END-->
@@ -108,25 +108,28 @@
 
         <div class="form-row">
             <div class="form-group col-md-6">
-                <label for="remitente" class="control-label">Adjunto Convenio</label>
-                <input type="file" id="archivo_convenio" name="archivo_convenio" accept="application/pdf" class="form-control">
+                <label for="status">ARCHIVO DE CONVENIO</label>
+                <div class="custom-file">
+                    <input type="file" id="archivo_convenio" name="archivo_convenio" accept="application/pdf" class="custom-file-input">
+                    <label for="archivo_convenio" class="custom-file-label">ARCHIVO CONVENIO</label>
+                </div>
             </div>
-
             <div class="form-group col-md-6">
-                <label for="status">Estatus</label>
-                <select class="form-control" id="status" name="status">
-                    <option value="true">Activo</option>
-                    <option value="false">Terminado</option>
-                </select>
-            </div>
-        </div>
-        <div class="form-row">
-            <div class="form-group col-md-6">
-                <label for="direccion" class="control-label">Dirección</label>
+                <label for="direccion" class="control-label">DIRECCIÓN</label>
                 <textarea name="direccion" class="form-control" id="direccion"></textarea>
             </div>
         </div>
-        <button type="submit" class="btn btn-primary">Guardar</button>
+        <!--botones de enviar y retroceder-->
+        <div class="row">
+            <div class="col-lg-12 margin-tb">
+                <div class="pull-left">
+                    <a class="btn btn-danger" href="{{URL::previous()}}">Regresar</a>
+                </div>
+                <div class="pull-right">
+                    <button type="submit" class="btn btn-primary" >Guardar</button>
+                </div>
+            </div>
+        </div>
     </form>
     <br>
  </div>
@@ -134,12 +137,13 @@
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
  <script>
     $( function() {
-      var dateFormat = "mm/dd/yy",
+      var dateFormat = "dd-mm-yy",
         from = $( "#fecha_firma" )
           .datepicker({
             defaultDate: "+1w",
             changeMonth: true,
-            numberOfMonths: 1
+            numberOfMonths: 1,
+            dateFormat: 'dd-mm-yy'
           })
           .on( "change", function() {
             to.datepicker( "option", "minDate", getDate( this ) );
@@ -147,7 +151,8 @@
         to = $( "#fecha_termino" ).datepicker({
           defaultDate: "+1w",
           changeMonth: true,
-          numberOfMonths: 1
+          numberOfMonths: 1,
+          dateFormat: 'dd-mm-yy'
         })
         .on( "change", function() {
           from.datepicker( "option", "maxDate", getDate( this ) );
