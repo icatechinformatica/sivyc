826dbe5a276871ac361ffdf5634cd3e27e46727a
daniel mendez cruz <danielmendez.88@live.com>
2020-06-15 22:39:17 -0500

    modificacion de afolios y folio Controller

--- app/Http/Controllers/ApiController/AfoliosController.php
+++ app/Http/Controllers/ApiController/AfoliosController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\ApiController;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Afolios;
+use App\Models\api\Afolios;
 
 class AfoliosController extends Controller
 {
--- app/Http/Controllers/ApiController/FolioController.php
+++ app/Http/Controllers/ApiController/FolioController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\ApiController;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Folio;
+use App\Models\api\Folio;
 
 class FolioController extends Controller
 {
