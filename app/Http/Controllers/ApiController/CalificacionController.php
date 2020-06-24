b6b32fce2bfca49b76b3464d74da004803b66fd6
daniel mendez cruz <danielmendez.88@live.com>
2020-06-24 12:37:47 -0500

    modificaciones de calificacionesApi rest

--- app/Http/Controllers/ApiController/CalificacionController.php
+++ app/Http/Controllers/ApiController/CalificacionController.php
@@ -106,12 +106,13 @@ class CalificacionController extends Controller
         //
         // actualizando
         try {
-            $Calificacion = new Calificacion();
-            $Calificacion->WHERE([
-                ['idcurso', '=', $request->idcurso],
-                ['matricula', '=', $request->matricula],
-            ])->update($request->all());
-            return response()->json(['success' => 'Calificacion actualizada exitosamente'], 200);
+            $solicitud_request = $request->all();
+            // $Calificacion = new Calificacion();
+            // $Calificacion->WHERE([
+            //     ['idcurso', '=', $request->idcurso],
+            //     ['matricula', '=', $request->matricula],
+            // ])->update($request->all());
+            return response()->json(['success' => $solicitud_request], 200);
         } catch(Exception $e) {
             return response()->json(['error' => $e->getMessage()], 501);
         }
