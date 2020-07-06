7a1de68c8859565442915666097cfef85c0ee4d4
daniel mendez cruz <danielmendez.88@live.com>
2020-07-06 12:37:45 -0500

    modificacion InstructoresController

--- app/Http/Controllers/ApiController/InstructoresController.php
+++ app/Http/Controllers/ApiController/InstructoresController.php
@@ -16,6 +16,7 @@ class InstructoresController extends Controller
     public function index()
     {
         $instructores = Instructor::WHERE('instructores.status', '=', 'Validado')
+        ->LEFTJOIN('instructor_perfil', 'instructor_perfil.numero_control', '=', 'instructores.id')
         ->LEFTJOIN('especialidades', 'especialidades.id', '=', 'instructores.id_especialidad')
         ->LEFTJOIN('tbl_unidades', 'tbl_unidades.cct', '=', 'instructores.clave_unidad')->GET([
             'instructores.id', 'instructores.numero_control', 'instructores.nombre', 'instructores.apellidoPaterno', 'instructores.apellidoMaterno',
@@ -25,7 +26,9 @@ class InstructoresController extends Controller
             'instructores.tipo_honorario', 'instructores.archivo_ine', 'instructores.archivo_domicilio', 'instructores.archivo_curp',
             'instructores.archivo_alta', 'instructores.archivo_bancario', 'instructores.archivo_fotografia', 'instructores.archivo_estudios',
             'instructores.archivo_otraid', 'instructores.status', 'instructores.rechazo', 'instructores.clave_unidad',
-            'especialidades.nombre AS nombre_especialidad', 'tbl_unidades.unidad AS unidades', 'instructores.motivo'
+            'especialidades.nombre AS nombre_especialidad', 'tbl_unidades.unidad AS unidades', 'instructores.motivo',
+            'instructor_perfil.area_carrera', 'instructor_perfil.grado_profesional', 'instructor_perfil.cursos_recibidos',
+            'instructor_perfil.estandar_conocer', 'instructor_perfil.registro_stps'
         ]);
         return response()->json($instructores, 200);
     }
