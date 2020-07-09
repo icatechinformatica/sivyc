a31f46f825ad4019815f076a6bb886edcf00e4c4
daniel mendez cruz <danielmendez.88@live.com>
2020-07-09 15:00:43 -0500

    modificaciones de InstructoresController

--- app/Http/Controllers/ApiController/InstructoresController.php
+++ app/Http/Controllers/ApiController/InstructoresController.php
@@ -19,7 +19,8 @@ class InstructoresController extends Controller
         ->LEFTJOIN('instructor_perfil', 'instructor_perfil.numero_control', '=', 'instructores.id')
         ->LEFTJOIN('especialidades', 'especialidades.id', '=', 'instructores.id_especialidad')
         ->LEFTJOIN('tbl_unidades', 'tbl_unidades.cct', '=', 'instructores.clave_unidad')
-        ->LEFTJOIN('especialidad_instructores', 'especialidad_instructores.perfilprof_id', '=', 'instructor_perfil.id')->GET([
+        ->LEFTJOIN('especialidad_instructores', 'especialidad_instructores.perfilprof_id', '=', 'instructor_perfil.id')
+        ->GET([
             'instructores.id', 'instructores.numero_control', 'instructores.nombre', 'instructores.apellidoPaterno', 'instructores.apellidoMaterno',
             'instructores.rfc', 'instructores.curp', 'instructores.sexo', 'instructores.estado_civil', 'instructores.fecha_nacimiento', 'instructores.entidad', 'instructores.municipio',
             'instructores.asentamiento', 'instructores.domicilio', 'instructores.telefono', 'instructores.correo', 'instructores.banco', 'instructores.no_cuenta',
@@ -29,7 +30,7 @@ class InstructoresController extends Controller
             'instructores.archivo_otraid', 'instructores.status', 'instructores.rechazo', 'instructores.clave_unidad',
             'especialidades.nombre AS nombre_especialidad', 'tbl_unidades.unidad AS unidades', 'instructores.motivo',
             'instructor_perfil.area_carrera', 'instructor_perfil.grado_profesional', 'instructor_perfil.cursos_recibidos',
-            'instructor_perfil.estandar_conocer', 'instructor_perfil.registro_stps', 'especialidad_insctructores.memorandum_validacion',
+            'instructor_perfil.estandar_conocer', 'instructor_perfil.registro_stps', 'especialidad_instructores.memorandum_validacion',
             'instructor_perfil.estatus'
         ]);
         return response()->json($instructores, 200);
