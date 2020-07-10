46e7cbcb1c1b722a2a811ae003075ba7e79d17a5
daniel mendez cruz <danielmendez.88@live.com>
2020-07-10 14:35:04 -0500

    modificaciones apirest AlumnoRegistradoController

--- app/Http/Controllers/ApiController/AlumnoRegistradoController.php
+++ app/Http/Controllers/ApiController/AlumnoRegistradoController.php
@@ -24,20 +24,22 @@ class AlumnoRegistradoController extends Controller
                 ->LEFTJOIN('alumnos_pre', 'alumnos_pre.id', '=', 'alumnos_registro.id_pre')
                 ->LEFTJOIN('tbl_unidades', 'alumnos_registro.unidad', '=', 'tbl_unidades.unidad')
                 ->ORDERBY('id_registro', 'desc')
-                ->GET(['alumnos_pre.nombre AS nombrealumno', 'alumnos_pre.apellido_paterno', 'alumnos_pre.apellido_materno', 'alumnos_pre.correo', 'alumnos_pre.telefono',
-                'alumnos_pre.curp AS curp_alumno', 'alumnos_pre.sexo', 'alumnos_pre.fecha_nacimiento', 'alumnos_pre.domicilio', 'alumnos_pre.colonia', 'alumnos_pre.cp', 'alumnos_pre.municipio',
-                'alumnos_pre.estado', 'alumnos_pre.estado_civil', 'alumnos_pre.discapacidad', 'alumnos_registro.no_control',
-                'alumnos_registro.id', 'alumnos_pre.ultimo_grado_estudios', 'alumnos_pre.empresa_trabaja',
-                'alumnos_pre.antiguedad', 'alumnos_pre.direccion_empresa', 'alumnos_pre.medio_entero', 'alumnos_registro.horario', 'alumnos_registro.grupo', 'alumnos_registro.grupo',
-                'alumnos_pre.chk_acta_nacimiento', 'alumnos_pre.chk_curp', 'alumnos_pre.chk_comprobante_domicilio',
-                'alumnos_pre.chk_comprobante_domicilio', 'alumnos_pre.chk_fotografia',
-                'alumnos_pre.chk_ine', 'alumnos_pre.chk_pasaporte_licencia', 'alumnos_pre.chk_comprobante_ultimo_grado',
-                'alumnos_pre.acta_nacimiento', 'alumnos_pre.curp',
-                'alumnos_pre.comprobante_domicilio', 'alumnos_pre.fotografia', 'alumnos_pre.ine', 'alumnos_pre.pasaporte_licencia_manejo',
-                'alumnos_pre.comprobante_ultimo_grado', 'alumnos_pre.chk_comprobante_calidad_migratoria',
-                'alumnos_pre.comprobante_calidad_migratoria', 'alumnos_pre.puesto_empresa', 'alumnos_pre.sistema_capacitacion_especificar',
-                'cursos.nombre_curso', 'especialidades.nombre AS especialidad', 'tbl_unidades.cct', 'alumnos_registro.id AS id_registro',
-                'alumnos_registro.unidad']);
+                ->GET([
+                    'alumnos_pre.nombre AS nombrealumno', 'alumnos_pre.apellido_paterno', 'alumnos_pre.apellido_materno', 'alumnos_pre.correo', 'alumnos_pre.telefono',
+                    'alumnos_pre.curp AS curp_alumno', 'alumnos_pre.sexo', 'alumnos_pre.fecha_nacimiento', 'alumnos_pre.domicilio', 'alumnos_pre.colonia', 'alumnos_pre.cp', 'alumnos_pre.municipio',
+                    'alumnos_pre.estado', 'alumnos_pre.estado_civil', 'alumnos_pre.discapacidad', 'alumnos_registro.no_control',
+                    'alumnos_registro.id', 'alumnos_pre.ultimo_grado_estudios', 'alumnos_pre.empresa_trabaja',
+                    'alumnos_pre.antiguedad', 'alumnos_pre.direccion_empresa', 'alumnos_pre.medio_entero', 'alumnos_registro.horario', 'alumnos_registro.grupo', 'alumnos_registro.grupo',
+                    'alumnos_pre.chk_acta_nacimiento', 'alumnos_pre.chk_curp', 'alumnos_pre.chk_comprobante_domicilio',
+                    'alumnos_pre.chk_comprobante_domicilio', 'alumnos_pre.chk_fotografia',
+                    'alumnos_pre.chk_ine', 'alumnos_pre.chk_pasaporte_licencia', 'alumnos_pre.chk_comprobante_ultimo_grado',
+                    'alumnos_pre.acta_nacimiento', 'alumnos_pre.curp',
+                    'alumnos_pre.comprobante_domicilio', 'alumnos_pre.fotografia', 'alumnos_pre.ine', 'alumnos_pre.pasaporte_licencia_manejo',
+                    'alumnos_pre.comprobante_ultimo_grado', 'alumnos_pre.chk_comprobante_calidad_migratoria',
+                    'alumnos_pre.comprobante_calidad_migratoria', 'alumnos_pre.puesto_empresa', 'alumnos_pre.sistema_capacitacion_especificar',
+                    'cursos.nombre_curso', 'especialidades.nombre AS especialidad', 'tbl_unidades.cct', 'alumnos_registro.id AS id_registro',
+                    'alumnos_registro.unidad', 'alumnos_registro.etnia', 'alumnos_registro.indigena', 'alumnos_registro.migrante'
+                ]);
 
         return response()->json($alumnos, 200);
     }
