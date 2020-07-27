001c4ae8a25b9db93c1cedb25169e4f7833d57bd
daniel mendez cruz <danielmendez.88@live.com>
2020-07-27 11:12:18 -0500

    modificaciones en AlumnoRegistradoController

--- app/Http/Controllers/webController/AlumnoRegistradoController.php
+++ app/Http/Controllers/webController/AlumnoRegistradoController.php
@@ -209,7 +209,7 @@ class AlumnoRegistradoController extends Controller
         $pdf = PDF::loadView('layouts.pdfpages.registroalumno', compact('alumnos', 'edad','date','pathimg'));
         // (Optional) Setup the paper size and orientation
         $pdf->setPaper('A4', 'portrait');
-        return $pdf->download('documento_sid_'.$noControl.'.pdf');
+        return $pdf->download('documento_sid_'.$alumnos->no_control.'.pdf');
 
         //return view('layouts.pdfpages.registroalumno', compact('alumnos','edad','date'));
     }
