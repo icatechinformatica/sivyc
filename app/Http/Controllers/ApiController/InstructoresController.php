b43966016fb55b0afcc3ebb91d67ab3d956c1b8c
daniel mendez cruz <danielmendez.88@live.com>
2020-07-27 23:18:43 -0500

    modificacion ApiController InstructoresCont

--- app/Http/Controllers/ApiController/InstructoresController.php
+++ app/Http/Controllers/ApiController/InstructoresController.php
@@ -19,7 +19,7 @@ class InstructoresController extends Controller
         ->LEFTJOIN('instructor_perfil', 'instructor_perfil.numero_control', '=', 'instructores.id')
         ->LEFTJOIN('tbl_unidades', 'tbl_unidades.cct', '=', 'instructores.clave_unidad')
         ->LEFTJOIN('especialidad_instructores', 'especialidad_instructores.perfilprof_id', '=', 'instructor_perfil.id')
-        ->LEFTJOIN('especialidad_instructores', 'especialidad_instructores.especialidad_id', '=', 'especialidades.id')
+        ->LEFTJOIN('especialidades', 'especialidades.id', '=', 'especialidad_instructores.especialidad_id')
         ->GET([
             'instructores.id', 'instructores.numero_control', 'instructores.nombre', 'instructores.apellidoPaterno', 'instructores.apellidoMaterno',
             'instructores.rfc', 'instructores.curp', 'instructores.sexo', 'instructores.estado_civil', 'instructores.fecha_nacimiento', 'instructores.entidad', 'instructores.municipio',
@@ -31,7 +31,7 @@ class InstructoresController extends Controller
             'tbl_unidades.unidad AS unidades', 'instructores.motivo',
             'instructor_perfil.area_carrera', 'instructor_perfil.grado_profesional', 'instructor_perfil.cursos_recibidos',
             'instructor_perfil.estandar_conocer', 'instructor_perfil.registro_stps', 'especialidad_instructores.memorandum_validacion',
-            'instructor_perfil.estatus', 'especialidad_instructores.nombre AS nombre_especialidad'
+            'instructor_perfil.estatus',  'especialidad_instructores.nombre AS nombre_especialidad'
         ]);
         return response()->json($instructores, 200);
     }
