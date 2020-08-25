fd9465e8518bd2eb708c5588938b32a1c312ad27
daniel mendez cruz <danielmendez.88@live.com>
2020-08-25 14:26:44 -0500

    adicion de Modelos

---
+++ app/Models/Inscripcion.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Inscripcion extends Model
+{
+    //
+    protected $table = 'tbl_inscripcion';
+
+    protected $fillable = [
+        'id','unidad','matricula','alumno','id_curso','curso',
+        'instructor', 'inicio', 'termino', 'hinicio', 'hfin', 'tinscripcion', 'abrinscri', 'munidad', 'costo'
+        ,'motivo', 'status', 'realizo'
+    ];
+
+    protected $hidden = ['created_at', 'updated_at'];
+}
