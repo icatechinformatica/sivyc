e7af402b99fc09d97f76faf62148d06f7dedd3ac
daniel mendez cruz <danielmendez.88@live.com>
2020-09-16 13:32:10 -0500

    modificacion de InstructorController, curso, especialidad_instructor, frmaddespecialidad, frmmodespecialidad

--- app/Models/especialidad_instructor.php
+++ app/Models/especialidad_instructor.php
@@ -12,17 +12,14 @@ class especialidad_instructor extends Model
 
     protected $fillable = [
         'id','especialidad_id','perfilprof_id','unidad_solicita','memorandum_valdidacion',
-        'fecha_validacion','memorandum_modificacion','observacion',
+        'fecha_validacion','memorandum_modificacion','observacion', 'criterio_pago_id'
     ];
 
     protected $hidden = ['created_at', 'updated_at'];
 
     public function cursos()
     {
-        return $this->belongsToMany(curso::class, 'especialidad_instructor_curso', 'id_especialidad_instructor', 'curso_id')->withPivot([
-            'pago_id',
-            'zona',
-        ]);
+        return $this->belongsToMany(curso::class, 'especialidad_instructor_curso', 'id_especialidad_instructor', 'curso_id');
     }
 
 }
