82ebf6c952d3253e555f70981933fde05ac3bdec
daniel mendez cruz <danielmendez.88@live.com>
2020-10-23 08:47:41 -0500

    modificaciones de supervisionController/AlumnoCont

--- app/Http/Controllers/supervisionController/AlumnoController.php
+++ app/Http/Controllers/supervisionController/AlumnoController.php
@@ -18,34 +18,34 @@ class AlumnoController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-     
+
     function __construct() {
-        
+
     }
-    
+
     public function index(Request $request){
-        
-    }    
-    
+
+    }
+
     public function lista(Request $request){
-       $id = $request->get("id");       
+       $id = $request->get("id");
        $data = DB::table('tbl_inscripcion')->select('id','alumno')->where('id_curso',$id)->get();
-       $str = "";              
-       foreach($data as $item){            
+       $str = "";
+       foreach($data as $item){
             $str .= "<a href='#' onclick='generarURL(".$item->id.",\"alumno\");' class='list-group-item list-group-item-action' data-toggle='modal'  data-target='#modalURL'>".$item->alumno."</a>";
        }
        if(!$str) $str = "NO SE ENCONTR&Oacute; REGISTROS QUE MOSTRAR";
        return $str;
-    } 
-      
-       
+    }
+
+
     public function revision($id)
     {
         session_start();
-        $_SESSION["id"] = $id; 
+        $_SESSION["id"] = $id;
         $fecha = date('d/m/Y');
         $data = alumno::Filtrar('id_tbl_cursos',$id)
-        ->PAGINATE(5, ['id','nombre','apellidoPaterno','apellidoMaterno','edad','escolaridad',
+        ->PAGINATE(5, ['id','nombre','apellido_paterno','apellido_materno','edad','escolaridad',
     'fecha_inscripcion','documentos','curso','numero_apertura','fecha_autorizacion','modalidad',
     'fecha_inicio','fecha_termino','hinicio','hfin','tipo','lugar','cuota',
     'ok_nombre','ok_edad','ok_escolaridad','ok_fecha_inscripcion','ok_documentos',
@@ -56,11 +56,11 @@ class AlumnoController extends Controller
     'obs_fecha_termino','obs_horario','obs_tipo','obs_lugar','obs_cuota','comentarios',
     'id_tbl_cursos','enviado','created_at']);
         $curso = DB::table('tbl_cursos')->where('id', $id)->first();
-        
+
         //alumnos_pre.id=alumnos_registros.id_pre y alumnos_registro.no_control=tbl_inscripciones.matricula
         //$inscripcion = DB::table('tbl_inscripcion as i')->first();
-        
-        
+
+
         $inscripcion = DB::table('tbl_inscripcion as insc')->select(
             DB::raw("date_part('year',age(a_pre.fecha_nacimiento)) as edad"),'a_pre.ultimo_grado_estudios','insc.alumno','insc.costo',
             'a_pre.chk_acta_nacimiento','a_pre.chk_curp','a_pre.comprobante_domicilio','a_pre.chk_fotografia','a_pre.chk_ine',
@@ -72,22 +72,22 @@ class AlumnoController extends Controller
             ->groupby('insc.id')->groupby('a_pre.fecha_nacimiento')->groupby('a_pre.ultimo_grado_estudios')
             ->groupby('insc.alumno')->groupby('a_pre.chk_acta_nacimiento')->groupby('a_pre.chk_curp')->groupby('a_pre.comprobante_domicilio')->groupby('a_pre.chk_fotografia')
             ->groupby('a_pre.chk_ine')->groupby('a_pre.chk_pasaporte_licencia')->groupby('a_pre.chk_comprobante_ultimo_grado')
-            ->groupby('a_pre.chk_comprobante_calidad_migratoria')->groupby('insc.costo')                        
-            ->first();         
-        
+            ->groupby('a_pre.chk_comprobante_calidad_migratoria')->groupby('insc.costo')
+            ->first();
+
         $anio = date("Y");
         $path_dir  =  storage_path().'/app/public/supervisiones/'.$anio.'/alumnos/';
-        $path_file = 'storage/supervisiones/'.$anio.'/alumnos/';       
-        
+        $path_file = 'storage/supervisiones/'.$anio.'/alumnos/';
+
         return view('supervision.escolar.alumno', compact('data','fecha','curso','inscripcion','path_dir','path_file'));
     }
-    
+
     public function update(Request $request)
-    {   
+    {
          session_start();
-        $id = $_SESSION["id"]; 
-        $idsupervision = $request->get('id_supervision');        
-        $boton = $request->get('boton'); 
+        $id = $_SESSION["id"];
+        $idsupervision = $request->get('id_supervision');
+        $boton = $request->get('boton');
         $app= alumno::find($idsupervision);
         switch($boton){
             case 'eliminar':/*
@@ -96,37 +96,37 @@ class AlumnoController extends Controller
                     $mensaje = 'Operacion Exitosa!';
                 else */
                     $mensaje = 'Operacion no se ha efectuado correctamente, por favor vuelver intentar.';
-                    
+
             break;
-            case 'enviar':                
+            case 'enviar':
                 $fieldsOK = ['ok_nombre','ok_edad','ok_escolaridad',
                 'ok_fecha_inscripcion','ok_documentos','ok_curso',
                 'ok_numero_apertura','ok_fecha_autorizacion','ok_modalidad',
                 'ok_fecha_inicio','ok_fecha_termino','ok_horario','ok_tipo',
-                'ok_lugar','ok_cuota'];        
-                 
+                'ok_lugar','ok_cuota'];
+
                 $fieldsOBS = ['obs_nombre','obs_edad',
                 'obs_escolaridad','obs_fecha_inscripcion','obs_documentos',
                 'obs_curso','obs_numero_apertura','obs_fecha_autorizacion',
                 'obs_modalidad','obs_fecha_inicio','obs_fecha_termino',
-                'obs_horario','obs_tipo','obs_lugar','obs_cuota','comentarios'];        
-                 
-                foreach ($fieldsOK as $i => $value) {   
+                'obs_horario','obs_tipo','obs_lugar','obs_cuota','comentarios'];
+
+                foreach ($fieldsOK as $i => $value) {
                     if($request->get($value)) $app->$value = $request->get($value);
-                    else $app->$value = false; 
-                } 
-                foreach ($fieldsOBS as $i => $value2) {            
+                    else $app->$value = false;
+                }
+                foreach ($fieldsOBS as $i => $value2) {
                     $app->$value2 = strtoupper($request->get($value2));
-                }    
-                
-                
+                }
+
+
                 $app->enviado = 1;
                 if($app->save())$mensaje = 'Operacion Exitosa!';
                 else $mensaje = 'Operacion no se ha efectuado correctamente, por favor vuelver intentar.';
             break;
-        }        
-        
-      return redirect('supervision/alumno/revision/'.$id)->with(['mensaje'=>$mensaje]);    
+        }
+
+      return redirect('supervision/alumno/revision/'.$id)->with(['mensaje'=>$mensaje]);
     }
-    
+
 }
