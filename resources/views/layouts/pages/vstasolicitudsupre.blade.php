e1cea6d4ca7defa396c871d09641276a723b8268
Orlando Chávez <60703406+oorlandochavez@users.noreply.github.com>
2020-12-14 18:00:34 -0600

    14122020 04

--- resources/views/layouts/pages/vstasolicitudsupre.blade.php
+++ resources/views/layouts/pages/vstasolicitudsupre.blade.php
@@ -159,7 +159,7 @@
                                 <a class="btn btn-danger" id="supre_pdf" name="supre_pdf" href="#" target="_blank">Solicitud de Suficiencia Presupuestal</a><br>
                             </div>
                             <div style="text-align:center" class="form-group">
-                                <a class="btn btn-danger" id="anexo_pdf" name="anexo_pdf" href="#" target="_blank">Anexo Solicitud de Suficiencia Presupuestal</a><br>
+                                <a class="btn btn-danger" id="anexo_pdf" name="anexo_pdf" href="#" target="_blank">Anexo  Solicitud de Suficiencia Presupuestal</a><br>
                             </div>
                             <div style="text-align:center" class="form-group">
 <<<<<<< HEAD
