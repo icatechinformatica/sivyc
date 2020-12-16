8e6396b7268000b377c234496f0b30ec58d78c05
Orlando Chávez <60703406+oorlandochavez@users.noreply.github.com>
2020-12-16 14:28:41 -0600

    16122020 01 2 migrations 2020_12_16_202051_fieldav_instructores_table & 2020_12_16_201859_fieldav_cursos_table

---
+++ database/migrations/2020_12_16_202051_fieldav_instructores_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class FieldavInstructoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('instructores', function (Blueprint $table) {
+            $table->json('unidades_disponible')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('instructores', function (Blueprint $table) {
+            //
+        });
+    }
+}
