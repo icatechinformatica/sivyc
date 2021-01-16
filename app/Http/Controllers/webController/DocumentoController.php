51873f3c2bfe701171d675b35fb96c48f24a73de
danielmendez88 <daniboymendez@gmail.com>
2021-01-15 21:32:11 -0600

    modificaciones al guardado de archivos en sistema sivyc

---
+++ app/Http/Controllers/webController/DocumentoController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers\webController;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DocumentoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($slugpath ,$alumnos_id, $slug)
+    {
+        //
+        $file = storage_path("app\\".$slugpath."\\alumnos\\" . $alumnos_id . '\\' . $slug);
+        if (file_exists($file)) {
+            # si existe el archivo podemos avanzar
+            $headers = [
+                'Content-Type' => 'application/pdf'
+            ];
+            return response()->download($file, 'File Test', $headers, 'inline');
+        } else {
+            abort(404, 'Archivo no encontrado!');
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- config/filesystems.php
+++ config/filesystems.php
@@ -54,7 +54,12 @@ return [
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
-
+        'mydisk' => [
+            'driver' => 'local',
+            'root' => storage_path('app/filesUpload'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'private'
+        ],
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
