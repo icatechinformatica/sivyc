e464d51c933f8b1766ff0692a400558c40e58c96
victorDev6 <victor7rodriguez6@gmail.com>
2021-02-03 13:33:28 -0600

    Se agregan modificaciones al modulo convenios

--- resources/views/layouts/pages/frmconvenio.blade.php
+++ resources/views/layouts/pages/frmconvenio.blade.php
@@ -121,26 +121,35 @@
                     <input type='text' id="telefono_enlace" name="telefono_enlace" class="form-control">
                 </div>
             </div>
-            <!-- Fecha conclusion END-->
-            <!--poblacion-->
-            <div class="form-group col-md-4">
-                <label for="poblacion" class="control-label">POBLACIÓN</label>
-                <input type='text' id="poblacion" name="poblacion" class="form-control" />
-            </div>
-            <!--poblacion END-->
-        </div>
 
-        <div class="form-row">
-            <!--municipio-->
-            <div class="form-group col-md-4">
-                <label for="municipio" class="control-label">MUNICIPIO</label>
-                <input type='text' id="municipio" name="municipio" class="form-control" />
-            </div>
-            <!--municipio END-->
-            <!--nombre_titular-->
-            <div class="form-group col-md-4">
-                <label for="nombre_titular" class="control-label">NOMBRE DEL TITULAR</label>
-                <input type='text' id="nombre_titular" name="nombre_titular" class="form-control" />
+            <hr>
+
+            <div class="form-row">
+                {{-- archivo --}}
+                <div class="form-group col">
+                    <label for="status">ARCHIVO DE CONVENIO</label>
+                    <div class="custom-file">
+                        <input type="file" id="archivo_convenio" name="archivo_convenio" accept="application/pdf"
+                            class="custom-file-input">
+                        <label for="archivo_convenio" class="custom-file-label">ARCHIVO CONVENIO</label>
+                    </div>
+                </div>
+                <!-- Tipo de sector -->
+                <div class="form-group col">
+                    <label for="sector">SECTOR</label>
+                    <select class="form-control" id="sector" name="sector">
+                        <option value="">--SELECCIONAR--</option>
+                        <option value="PUBLICO">PUBLICO</option>
+                        <option value="PRIVADO">PRIVADO</option>
+                        <option value="SOCIAL">SOCIAL</option>
+                    </select>
+                </div>
+                {{-- btn publicar --}}
+                <div class="col text-center pt-4">
+                    <label for="publicar">PUBLICAR</label>
+                    <input type="checkbox" id="publicar" name="publicar" checked data-toggle="toggle" data-on="Si"
+                        data-off="No" data-onstyle="success" data-offstyle="danger" data-width="100" data-height="30">
+                </div>
             </div>
             <!--nombre_titular END-->
             <!--nombre_enlace-->
@@ -151,71 +160,213 @@
             <!--nombre_enlace END-->
         </div>
 
-        <div class="form-row">
-            <div class="form-group col-md-6">
-                <label for="status">ARCHIVO DE CONVENIO</label>
-                <div class="custom-file">
-                    <input type="file" id="archivo_convenio" name="archivo_convenio" accept="application/pdf" class="custom-file-input">
-                    <label for="archivo_convenio" class="custom-file-label">ARCHIVO CONVENIO</label>
+            <hr>
+            <div class="row">
+                <div class="col d-flex justify-content-center">
+                    <h1>Unidades</h1>
+                    <div class="col d-flex justify-content-end">
+                        <button id="btnMarcar" type="button" class="btn btn-link check-all">Marcar todos</button>
+                    </div>
                 </div>
             </div>
-            <div class="form-group col-md-6">
-                <label for="direccion" class="control-label">DIRECCIÓN</label>
-                <textarea name="direccion" class="form-control" id="direccion"></textarea>
+
+            <div class="row">
+                @foreach ($unidades as $unidad)
+                    <div class="col-4">
+                        <div class="form-group">
+                            <div class="custom-control custom-checkbox">
+                                <input type="checkbox" value="{{ $unidad->unidad }}" class="custom-control-input settings"
+                                    name="unidades[]" id="check + {{ $unidad->id }}">
+                                <label class="custom-control-label"
+                                    for="check + {{ $unidad->id }}">{{ $unidad->unidad }}</label>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
             </div>
-        </div>
-        <!--botones de enviar y retroceder-->
-        <div class="row">
-            <div class="col-lg-12 margin-tb">
-                <div class="pull-left">
-                    <a class="btn btn-danger" href="{{URL::previous()}}">Regresar</a>
-                </div>
-                @can('convenios.store')
-                    <div class="pull-right">
-                        <button type="submit" class="btn btn-primary" >Guardar</button>
+
+            <!--botones de enviar y retroceder-->
+            <div class="row mt-5">
+                <div class="col-lg-12 margin-tb">
+                    <div class="pull-left">
+                        {{-- <a class="btn btn-danger" href="{{ URL::previous() }}">Regresar</a> --}}
+                        <a class="btn btn-danger" id="tbn">Regresar</a>
+
                     </div>
                 @endcan
             </div>
-        </div>
-    </form>
-    <br>
- </div>
+        </form>
+        <br>
+    </div>
 
- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
- <script>
-    $( function() {
-      var dateFormat = "dd-mm-yy",
-        from = $( "#fecha_firma" )
-          .datepicker({
-            defaultDate: "+1w",
-            changeMonth: true,
-            numberOfMonths: 1,
-            dateFormat: 'dd-mm-yy'
-          })
-          .on( "change", function() {
-            to.datepicker( "option", "minDate", getDate( this ) );
-          }),
-        to = $( "#fecha_termino" ).datepicker({
-          defaultDate: "+1w",
-          changeMonth: true,
-          numberOfMonths: 1,
-          dateFormat: 'dd-mm-yy'
-        })
-        .on( "change", function() {
-          from.datepicker( "option", "maxDate", getDate( this ) );
+@endsection
+
+@section('script_content_js')
+    <script src="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/js/bootstrap4-toggle.min.js"></script>
+
+    <script type="text/javascript">
+        $(function() {
+            $.validator.addMethod('filesize', function(value, element, param) {
+                return this.optional(element) || (element.files[0].size <= param)
+            }, 'El TAMAÑO DEL ARCHIVO DEBE SER MENOR A {0} bytes.');
+
+            // validaciones
+            $('#frmConvenio').validate({
+                rules: {
+                    no_convenio: {
+                        required: true
+                    },
+                    institucion: {
+                        required: true
+                    },
+                    nombre_titular: {
+                        required: true
+                    },
+                    telefono: {
+                        required: true
+                    },
+                    direccion: {
+                        required: true
+                    },
+                    poblacion: {
+                        required: true
+                    },
+                    tipo_convenio: {
+                        required: true
+                    },
+                    nombre_firma: {
+                        required: true
+                    },
+                    sector: {
+                        required: true
+                    },
+                    fecha_firma: {
+                        required: true
+                    },
+                    fecha_termino: {
+                        required: true
+                    },
+                    nombre_enlace: {
+                        required: true
+                    },
+                    telefono_enlace: {
+                        required: true
+                    },
+                    municipio: {
+                        required: true
+                    },
+                    status: {
+                        required: true
+                    },
+                    archivo_convenio: {
+                        extension: "pdf",
+                        filesize: 2000000, //max size 2mb
+                    }
+                },
+                messages: {
+                    no_convenio: {
+                        required: "Campo requerido"
+                    },
+                    institucion: {
+                        required: "Campo requerido"
+                    },
+                    telefono: {
+                        required: "Campo requerido"
+                    },
+                    sector: {
+                        required: "Campo requerido"
+                    },
+                    fecha_firma: {
+                        required: "Campo requerido"
+                    },
+                    fecha_termino: {
+                        required: "Campo requerido"
+                    },
+                    poblacion: {
+                        required: "Campo requerido"
+                    },
+                    municipio: {
+                        required: "Campo requerido"
+                    },
+                    tipo_convenio: {
+                        required: "Campo requerido"
+                    },
+                    nombre_firma: {
+                        required: "Campo requerido"
+                    },
+                    nombre_titular: {
+                        required: "Campo requerido"
+                    },
+                    nombre_enlace: {
+                        required: "Campo requerido"
+                    },
+                    telefono_enlace: {
+                        required: "Campo requerido"
+                    },
+                    status: {
+                        required: "Campo requerido"
+                    },
+                    direccion: {
+                        required: "Campo requerido"
+                    },
+                    archivo_convenio: {
+                        accept: "Sólo se permiten pdf",
+                        filesize: "El archivo debe ser menor de 2 MB",
+                    }
+                }
+            });
+
+            var dateFormat = "dd-mm-yy",
+                from = $("#fecha_firma")
+                .datepicker({
+                    defaultDate: "+1w",
+                    changeMonth: true,
+                    numberOfMonths: 1,
+                    dateFormat: 'dd-mm-yy'
+                })
+                .on("change", function() {
+                    to.datepicker("option", "minDate", getDate(this));
+                }),
+                to = $("#fecha_termino").datepicker({
+                    defaultDate: "+1w",
+                    changeMonth: true,
+                    numberOfMonths: 1,
+                    dateFormat: 'dd-mm-yy'
+                })
+                .on("change", function() {
+                    from.datepicker("option", "maxDate", getDate(this));
+                });
+
+            function getDate(element) {
+                var date;
+                try {
+                    date = $.datepicker.parseDate(dateFormat, element.value);
+                } catch (error) {
+                    date = null;
+                }
+
+                return date;
+            }
+
+            // switch
+            $('#chkToggle2').bootstrapToggle();
+
+            // checksBox
+            var checked = true;
+            $('.settings').prop('checked', true);
+            $('#btnMarcar').html('Desmarcar todos');
+
+            $('.check-all').on('click', function() {
+                if (checked == false) {
+                    $('.settings').prop('checked', true);
+                    checked = true;
+                    $('#btnMarcar').html('Desmarcar todos');
+                } else {
+                    $('.settings').prop('checked', false);
+                    checked = false;
+                    $('#btnMarcar').html('Marcar todos');
+                }
+            });
         });
-
-      function getDate( element ) {
-        var date;
-        try {
-          date = $.datepicker.parseDate( dateFormat, element.value );
-        } catch( error ) {
-          date = null;
-        }
-
-        return date;
-      }
-    } );
-</script>
 
 @stop
--- resources/views/layouts/pages/vstconvenios.blade.php
+++ resources/views/layouts/pages/vstconvenios.blade.php
@@ -1,102 +1,110 @@
 <!--Creado por Daniel Méndez-->
 @extends('theme.sivyc.layout')
+
 <!--llamar a la plantilla -->
 @section('title', 'Convenios | SIVyC Icatech')
-<!--seccion-->
+    <!--seccion-->
+
 @section('content')
     <style>
         * {
-        box-sizing: border-box;
+            box-sizing: border-box;
         }
 
         #myInput {
-        background-image: url('img/search.png');
-        background-position: 5px 10px;
-        background-repeat: no-repeat;
-        background-size: 32px;
-        width: 100%;
-        font-size: 16px;
-        padding: 12px 20px 12px 40px;
-        border: 1px solid #ddd;
-        margin-bottom: 12px;
+            background-image: url('img/search.png');
+            background-position: 5px 10px;
+            background-repeat: no-repeat;
+            background-size: 32px;
+            width: 100%;
+            font-size: 16px;
+            padding: 12px 20px 12px 40px;
+            border: 1px solid #ddd;
+            margin-bottom: 12px;
         }
+
     </style>
-    <div class="container g-pt-50">
+
+    <div class="container-fluid px-5 g-pt-30">
         @if ($message = Session::get('success'))
             <div class="alert alert-success">
                 <p>{{ $message }}</p>
             </div>
         @endif
+
         <div class="row">
             <div class="col-lg-12 margin-tb">
                 <div class="pull-left">
-                    <h2>Convenios</h2>
+                    <h1>Convenios</h1>
                 </div>
-                <br>
                 @can('convenios.create')
                     <div class="pull-right">
-                        <a class="btn btn-success btn-lg" href="{{route('convenio.create')}}">NUEVO</a>
+                        <a class="btn btn-success btn-lg" href="{{ route('convenio.create') }}">NUEVO</a>
                     </div>
                 @endcan
             </div>
         </div>
-        <hr style="border-color:dimgray">
-        <table  id="table-instructor" class="table table-bordered Datatables">
-            <caption>Catalogo de Solcitudes</caption>
+
+        <div class="form-row">
+            <div class="col">
+                {!! Form::open(['route' => 'convenios.index', 'method' => 'GET', 'class' => 'form-inline']) !!}
+                <select name="busqueda" class="form-control mr-sm-2" id="busqueda">
+                    <option value="">BUSCAR POR TIPO</option>
+                    <option value="no_convenio">N° DE CONVENIO</option>
+                    <option value="institucion">INSTITUCIÓN</option>
+                    <option value="tipo_convenio">TIPO DE CONVENIO</option>
+                    <option value="sector">SECTOR</option>
+                </select>
+
+                {!! Form::text('busqueda_conveniopor', null, ['class' => 'form-control mr-sm-2', 'placeholder' => 'BUSCAR',
+                'aria-label' => 'BUSCAR']) !!}
+                <button type="submit" class="btn btn-outline-primary">BUSCAR</button>
+                {!! Form::close() !!}
+            </div>
+        </div>
+
+        <table id="table-instructor" class="table table-bordered table-striped mt-5">
             <thead>
                 <tr>
                     <th scope="col">NO. DE CONVENIO</th>
                     <th scope="col">INSTITUCIÓN</th>
-                    <th scope="col">FECHA DE FIRMA</th>
-                    <th scope="col">FECHA DE VIGENCIA</th>
-                    <th scope="col">ESTADO</th>
-                    @can('convenios.show')
-                        <th scope="col">VER</th>
-                    @endcan
+                    <th width="150px">FECHA DE FIRMA</th>
+                    <th width="150px">FECHA DE VIGENCIA</th>
+                    <th width="150px">TIPO DE CONVENIO</th>
+                    <th width="150px">SECTOR</th>
                     @can('convenios.edit')
-                        <th width="160px">MODIFICAR</th>
+                        <th scope="col">MODIFICAR</th>
                     @endcan
                 </tr>
             </thead>
             <tbody>
                 @foreach ($data as $itemData)
                     <tr>
-                    <th scope="row">{{$itemData->no_convenio}}</th>
-                        <td>{{$itemData->institucion}}</td>
-                        <td>{{$itemData->fecha_firma}}</td>
-                        <td>{{$itemData->fecha_vigencia}}</td>
-                        <td>
-                           @if (isset($itemData->status))
-                            {{ ($itemData->status == true) ? "ACTIVO" : "TERMINADO"}}
-                           @endif
-                        </td>
-
-                        @can('convenios.show')
-                            <td>
-                                <a href="{{ route('convenios.show', ['id' => base64_encode($itemData->id)])}}"
-                                    class="btn btn-success btn-circle m-1 btn-circle-sm" data-toggle="tooltip" data-placement="top" title="DETALLES">
-                                    <i class="fa fa-eye" aria-hidden="true"></i>
-                                </a>
-                            </td>
-                        @endcan
+                        <td scope="row">{{ $itemData->no_convenio }}</td>
+                        <td>{{ $itemData->institucion }}</td>
+                        <td>{{ $itemData->fecha_firma }}</td>
+                        <td>{{ $itemData->fecha_vigencia }}</td>
+                        <td>{{ $itemData->tipo_convenio }}</td>
+                        <td>{{ $itemData->sector }}</td>
 
                         @can('convenios.edit')
-                        <td>
-                            <a class="btn btn-warning btn-circle m-1 btn-circle-sm" data-toggle="tooltip" data-placement="top" title="EDITAR CONVENIO"
-                                href="{{route('convenios.edit', ['id' => base64_encode($itemData->id) ])}}">
-                                <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
-                            </a>
-                        </td>
+                            <td>
+                                <a class="btn btn-warning btn-circle m-1 btn-circle-sm" data-toggle="tooltip"
+                                    data-placement="top" title="EDITAR CONVENIO"
+                                    href="{{ route('convenios.edit', ['id' => base64_encode($itemData->id)]) }}">
+                                    <i class="fa fa-pencil-square-o fa-2x mt-2" aria-hidden="true"></i>
+                                </a>
+                            </td>
                         @endcan
                     </tr>
                 @endforeach
             </tbody>
-            <tfoot>
-                <tr>
-                </tr>
-            </tfoot>
         </table>
-        <br>
     </div>
-    <br>
+
+    <div class="row py-4">
+        <div class="col d-flex justify-content-center">
+            {{ $data->links() }}
+        </div>
+    </div>
 @endsection
