b6cc9c4a7c6541003c9819b11de2de4dee1bf555
danielmendez88 <daniboymendez@gmail.com>
2021-03-16 00:15:01 -0600

    modificaciones de la ruta de DocumentoController.php

--- app/Http/Controllers/webController/DocumentoController.php
+++ app/Http/Controllers/webController/DocumentoController.php
@@ -47,7 +47,7 @@ class DocumentoController extends Controller
     public function show($folder, $id, $slug)
     {
         //
-        $file = storage_path("app\\filesUpload\\".$folder."\\" . $id . '\\' . $slug);
+        $file = storage_path("app\\storage\\filesUpload\\".$folder."\\" . $id . '\\' . $slug);
         if (file_exists($file)) {
             # si existe el archivo podemos avanzar
             $headers = [
