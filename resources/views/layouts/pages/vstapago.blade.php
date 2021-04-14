9087fb3bbc7c6a2fbff117577064902dd126094b
Orlando Chávez <60703406+oorlandochavez@users.noreply.github.com>
2021-04-14 11:17:03 -0500

    14042021 01 upgrade contrato restart

--- resources/views/layouts/pages/vstapago.blade.php
+++ resources/views/layouts/pages/vstapago.blade.php
@@ -120,6 +120,15 @@
                                             <i class="fa fa-eye" aria-hidden="true"></i>
                                         </a>
                                     @endcan
+                                    @can('contrato.restart')
+                                        <button type="button" class="btn btn-danger btn-circle m-1 btn-circle-sm"
+                                            data-toggle="modal" data-placement="top"
+                                            data-target="#restartModalContrato"
+                                            data-id='{{$itemData->id_folios}}'
+                                            title="Reiniciar Contrato">
+                                            <i class="fa fa-history"></i>
+                                        </button>
+                                    @endcan
                                 @break
                                 @case('Pago_Verificado')
                                     <a class="btn btn-danger btn-circle m-1 btn-circle-sm" title="PDF" id="show_pdf" name="show_pdf" data-toggle="modal" data-target="#myModal" data-id='["{{$itemData->id_folios}}","{{$itemData->id_contrato}}","{{$itemData->docs}}","{{$itemData->id_supre}}","{{$itemData->status}}","{{$itemData->doc_validado}}"]'>
@@ -151,6 +160,15 @@
                                         <a class="btn btn-success btn-circle m-1 btn-circle-sm" title="Modificar Solicitud de Pago" href="{{route('pago-mod', ['id' => $itemData->id_folios])}}" >
                                             <i class="fa fa-wrench" aria-hidden="true"></i>
                                         </a>
+                                    @endcan
+                                    @can('contrato.restart')
+                                        <button type="button" class="btn btn-danger btn-circle m-1 btn-circle-sm"
+                                            data-toggle="modal" data-placement="top"
+                                            data-target="#restartModalContrato"
+                                            data-id='{{$itemData->id_folios}}'
+                                            title="Reiniciar Contrato">
+                                            <i class="fa fa-history"></i>
+                                        </button>
                                     @endcan
                                 @break
                                 @case('Finalizado')
@@ -226,7 +244,7 @@
                         <button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>
                     </div>
                     <div class="form-group col-md-4">
-                        <a class="btn btn-success" id="confirm_restart" name="confirm_restart" href="#">Aceptar</a>
+                        <a class="btn btn-success" id="confirm_restart2" name="confirm_restart2" href="#">Aceptar</a>
                     </div>
                     <div class="form-group col-md-2"></div>
                 </div>
@@ -235,6 +253,30 @@
     </div>
 <!-- END -->
     <br>
+    <!-- Modal -->
+    <div class="modal fade" id="restartModalContrato" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title"><b>¿Esta seguro de reiniciar este proceso?</b></h5>
+                    <button type="button" class="close" data-dismiss="modal">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="form-row">
+                    <div class="form-group col-md-2"></div>
+                    <div class="form-group col-md-4">
+                        <button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>
+                    </div>
+                    <div class="form-group col-md-4">
+                        <a class="btn btn-success" id="confirm_restart" name="confirm_restart" href="#">Aceptar</a>
+                    </div>
+                    <div class="form-group col-md-2"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+<!-- END -->
 @endsection
 @section('script_content_js')
 <script src="{{ asset("js/validate/modals.js") }}"></script>
