91f564cb4614fe1e0d727c86b61b8224c1c86e7e
romelia <rpnanguelu@gmail.com>
2021-04-15 12:20:33 -0500

    ASIGANCION Y CANCELACION DE FOLIOS

--- app/Http/Controllers/Grupos/asignarfoliosController.php
+++ app/Http/Controllers/Grupos/asignarfoliosController.php
@@ -18,6 +18,7 @@ class asignarfoliosController extends Controller
     
     public function index(Request $request){
         $id_user = Auth::user()->id;
+        
         $rol = DB::table('role_user')->LEFTJOIN('roles', 'roles.id', '=', 'role_user.role_id')            
             ->WHERE('role_user.user_id', '=', $id_user)->WHERE('roles.slug', 'like', '%unidad%')
             ->value('roles.slug');        
@@ -35,7 +36,7 @@ class asignarfoliosController extends Controller
         if(session('clave')) $clave = session('clave');
         else $clave = $request->clave;
         if($clave) $_SESSION['clave'] = $clave;
-        $data = $this->validaCurso($clave);
+        $data = $this->validaCurso($clave, NULL);
         list($curso, $acta, $alumnos, $message) = $data; 
 
         if(session('message')) $message = session('message');
@@ -43,46 +44,63 @@ class asignarfoliosController extends Controller
         return  view('grupos.asignarfolios.index', compact('curso','alumnos','message','acta')); 
     } 
     
-    public function store() {     
+    public function store(Request $request) {     
+        $id_afolio = $request->id_afolio*1;
         $clave = $_SESSION['clave'];
-        $data = $this->validaCurso($clave);
-        list($curso, $acta, $alumnos, $message) = $data; 
+        $data = $this->validaCurso($clave, $id_afolio);
+        list($curso, $acta, $alumnos, $message) = $data; //var_dump($acta);exit;      
+        
         if($acta AND !$message){
-            $id_curso = $curso->id;
-            $acta->num_inicio;
-            $acta->num_fin;
-            $num_folio = $acta->contador;
-            foreach($alumnos as $a){   
-                if($a->calificacion =="NP"){
-                    $movimiento = "SIN EXPEDIR";
-                    $motivo= "NO ACREDITADO";
-                    $folio = 0;
-                }else{
-                    $movimiento = "EXPEDIDO";
-                    $motivo= "ACREDITADO";
-                   if($curso->mod=="EXT") $folio = "D";
-                   else $folio = "C";
-                   $folio .= str_pad($num_folio, 6, "0", STR_PAD_LEFT);   
-                }
-                             
-                $result = DB::table('tbl_folios')->insert(
-                    ['unidad' => $curso->unidad, 'id_curso'=>$curso->id,'matricula'=>$a->matricula, 'nombre'=>$a->alumno,
-                    'folio' => $folio, 'movimiento'=> $movimiento, 'motivo' => $motivo, 'mod'=> $curso->mod, 'fini' => $acta->finicial, 'ffin' => $acta->ffinal, 'focan' => 0,
-                    'fecha_acta' => $acta->facta, 'fecha_expedicion' => date('Y-m-d'), 'id_unidad' => $acta->id_unidad, 'id_afolios' => $acta->id, 'iduser_created' => Auth::user()->id 
-                    ]
-                
-                );
-                if($a->calificacion != "NP"){
-                    if($result) DB::table('tbl_afolios')->where('id',$acta->id)->increment('contador');                
-                    $num_folio++;
-                }
+            $id_curso = $curso->id;  
+            $num_folio = $acta->num_inicio+$acta->contador;// echo $num_folio;exit;
+               
+            foreach($alumnos as $a){  //var_dump($a);exit;
+                if($num_folio<=$acta->num_fin){
+                    if((!$a->folio AND $a->calificacion !="NP") OR ($a->movimiento=="CANCELADO" AND $a->reexpedicion==false))  {
+                        
+                        $motivo= "ACREDITADO";
+                        if($a->movimiento=="CANCELADO"){
+                            $reexpedicion=true;
+                            if($a->motivo=='ROBO O EXTRAVIO' OR $a->motivo=='NO SOLICITADO')$movimiento='DUPLICADO';
+                            else $movimiento='REEXPEDIDO';                            
+                        }else{
+                            $reexpedicion=false;                            
+                             $movimiento = "EXPEDIDO";
+                        }
+                        
+                        if($curso->mod=="EXT") $prefijo = "D";
+                        elseif($curso->mod=="CAE") $prefijo = "C";
+                        else $prefijo = "A";
+
+                        $folio = $prefijo.str_pad($num_folio, 6, "0", STR_PAD_LEFT);                       
+                       
+                        $id_folio = DB::table('tbl_folios')->insertGetId(
+                            ['unidad' => $curso->unidad, 'id_curso'=>$curso->id,'matricula'=>$a->matricula, 'nombre'=>$a->alumno,
+                                'folio' => $folio, 'movimiento'=> $movimiento, 'motivo' => $motivo, 'mod'=> $curso->mod, 'fini' => $acta->finicial, 'ffin' => $acta->ffinal, 'focan' => 0,
+                                'fecha_acta' => $acta->facta, 'fecha_expedicion' => date('Y-m-d'), 'id_unidad' => $acta->id_unidad, 'id_banco_folios' => $acta->id, 
+                                'iduser_created' => Auth::user()->id, 'realizo'=>Auth::user()->name 
+                                ]                            
+                         );
+                         
+                         $data = ['reexpedicion' => $reexpedicion, 'iduser_updated' => Auth::user()->id];
+                         if($movimiento!='DUPLICADO') $data['id_folio']= $id_folio;
+                         $resultAlumno = DB::table('tbl_inscripcion')->where('id',$a->id)->update($data);
+                  
+                                        
+                        if($id_folio){
+                                DB::table('tbl_banco_folios')->where('id',$acta->id)->lockForUpdate()->increment('contador');
+                                $message = "Operacion exitosa!!";
+                        }                
+                        $num_folio++;
+                    }           
+                     
+                }else $message = "Se han agotado los folios en esta Acta, volver a intentar!!";
             }
-            if($result) $message = "Operacion exitosa!!"; 
         }           
         return redirect('grupos/asignarfolios')->with(['message'=>$message, 'clave'=>$clave]);
     } 
 
-    private function validaCurso($clave){
+    private function validaCurso($clave, $id_afolio){
         $curso = $alumnos = $message = $acta = NULL;
         if($clave){  
             //EXISTE EL CURSO
@@ -91,21 +109,30 @@ class asignarfoliosController extends Controller
                 $curso = $curso->first();
             if($curso){
                 ///ACTA CON FOLIOS DISPONIBLES
-                $acta =  DB::table('tbl_afolios')->where('unidad',$curso->unidad)->where('mod',$curso->mod)->where('activo',true)->whereColumn('contador','<','num_fin')->first();
-                if(!$acta)$message = "No hay Acta con Folios disponibles. ";
-                ///NO DEBE TENER FOLIOS ASIGNADOS
-                $folios =  DB::table('tbl_folios')->where('id_curso',$curso->id)->where('movimiento','<>','CANCELADO')->where('folio','>','0')->first();
-                if($folios)$message = "Curso con folios asignados. ";
+                if($curso->mod=="EXT" OR $curso->mod=="CAE" ) $mod = $curso->mod;
+                else $mod = "GRAL";
+                $acta =  DB::table('tbl_banco_folios')->where('unidad',$curso->unidad)->where('mod',$mod)
+                    ->where('activo',true)->whereColumn('contador','<','total');
+                    if($id_afolio){
+                        $acta =  $acta->where('id',$id_afolio)->first(); //solo un folio
+                        if(!$acta) $message = "No hay Acta con Folios disponibles. ";
+                    }else{
+                        $acta =  $acta->orderby('id')->get(); //todos los folios
+                        if(count($acta)==0) $message = "No hay Acta con Folios disponibles. ";
+                    }
+                
                 ///ALUMNOS REGISTRADOS
-                $alumnos = DB::table('tbl_inscripcion as i')->select('i.id','i.matricula','i.alumno','i.calificacion','f.folio','f.fecha_expedicion','f.motivo')
-                    ->leftjoin('tbl_folios as f', function ($join) {                        
-                        $join->on('f.id_curso','=','i.id_curso')
-                        ->on('f.matricula','=','i.matricula')
-                        ->where('f.movimiento','<>','CANCELADO');
-                    })->where('i.id_curso',$curso->id)->orderby('i.alumno')->get();                  
-               
+                $alumnos = DB::table('tbl_inscripcion as i')->select('i.id','i.matricula','i.alumno','i.calificacion','i.reexpedicion','f.folio','f.fecha_expedicion','f.movimiento','f.motivo')
+                    //->leftjoin('tbl_folios as f','f.id','i.id_folio')
+                    ->leftJoin('tbl_folios as f', function($join){                                        
+                        $join->on('f.id_curso', '=', 'i.id_curso');
+                        $join->on('f.matricula', '=', 'i.matricula');                            
+                    }) 
+                    ->where('i.id_curso',$curso->id)->orderby('i.alumno')->get();                  
+               //var_dump($alumnos);exit;
                 if(count($alumnos)==0) $message = "El curso no tiene alumnos registrados. ";
                 elseif(count($alumnos)>0) if(!$alumnos[0]->calificacion)$message = "No hay registro de calificaciones, no podrá asignar folios. ";
+                //elseif(count($alumnos)>0) if($alumnos[0]->folio)$message = "Curso con folios expedidos. ";
                 if(!$message)  $_SESSION['clave'] = $curso->clave; 
                 
             }else $message = "Clave inválida.";
