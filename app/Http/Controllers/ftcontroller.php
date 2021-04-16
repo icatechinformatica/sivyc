941a001c0de6a7fc459b36ea4f293e6f15f8b86b
danielmendez88 <daniboymendez@gmail.com>
2021-04-16 12:01:01 -0500

    modificaciones en ftcontroller.php by slug

--- app/Http/Controllers/ftcontroller.php
+++ app/Http/Controllers/ftcontroller.php
@@ -537,8 +537,8 @@ class ftcontroller extends Controller
                     $total=count($_POST['chkcursos_list']);          
                     $id_user = Auth::user()->id;
                     $rol = DB::table('role_user')->select('roles.slug')->leftjoin('roles', 'roles.id', '=', 'role_user.role_id') 
-                    ->where([['role_user.user_id', '=', $id_user], ['roles.slug', '=', 'unidad']])->get();
-                    if($rol[0]->slug=='unidad'){ 
+                    ->where([['role_user.user_id', '=', $id_user], ['roles.slug', 'like', '%unidad%']])->get();
+                    if(count($rol) > 0){ 
                         $unidad = Auth::user()->unidad;
                         $unidad = DB::table('tbl_unidades')->where('id',$unidad)->value('unidad');
                         $_SESSION['unidad'] = $unidad;
