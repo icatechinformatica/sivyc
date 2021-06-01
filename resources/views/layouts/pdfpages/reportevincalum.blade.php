660d2c337040d1637504a23d6bf7a7f75fcd4272
Orlando Chávez <60703406+oorlandochavez@users.noreply.github.com>
2021-06-01 12:49:11 -0500

    01062021 02 mini fix reporte vinculador-alumni

--- resources/views/layouts/pdfpages/reportevincalum.blade.php
+++ resources/views/layouts/pdfpages/reportevincalum.blade.php
@@ -99,49 +99,49 @@
                 <table width="700" class="table table-striped" id="table-one">
                     <thead>
                         <tr>
-                            <td scope="col"><small style="font-size: 8px;">Sec. de Solic.</small></td>
-                            <td scope="col"><small style="font-size: 8px;">UNIDAD</small></td>
-                            <td scope="col"><small style="font-size: 8px;">ALUMNO</small></td>
-                            <td scope="col"><small style="font-size: 8px;">MATRICULA</small></td>
-                            <td width="8px"><small style="font-size: 8px;">CURP</small></td>
-                            <td scope="col"><small style="font-size: 8px;">SEXO</small></td>
-                            <td scope="col"><small style="font-size: 8px;">ESPECIALIDAD</small></td>
-                            <td scope="col"><small style="font-size: 8px;">CURSO</small></td>
-                            <td scope="col"><small style="font-size: 8px;">CLAVE DE CURSO</small></td>
-                            <td scope="col"><small style="font-size: 8px;">MODALIDAD</small></td>
-                            <td scope="col"><small style="font-size: 8px;">TIPO DE CURSO</small></td>
-                            <td scope="col"><small style="font-size: 8px;">INSCRITO SICE</small></td>
-                            <td scope="col"><small style="font-size: 8px;">INSCRITO SIVYC<small></td>
-                            <td scope="col"><small style="font-size: 8px;">REALIZO SICE<small></td>
-                            <td scope="col"><small style="font-size: 8px;">REALIZO SIVYC<small></td>
+                            <td scope="col"><small style="font-size: 10px;">Sec. de Solic.</small></td>
+                            <td scope="col"><small style="font-size: 10px;">UNIDAD</small></td>
+                            <td scope="col"><small style="font-size: 10px;">ALUMNO</small></td>
+                            <td scope="col"><small style="font-size: 10px;">MATRICULA</small></td>
+                            <td width="10px"><small style="font-size: 10px;">CURP</small></td>
+                            <td scope="col"><small style="font-size: 10px;">SEXO</small></td>
+                            <td scope="col"><small style="font-size: 10px;">ESPECIALIDAD</small></td>
+                            <td scope="col"><small style="font-size: 10px;">CURSO</small></td>
+                            <td scope="col"><small style="font-size: 10px;">CLAVE DE CURSO</small></td>
+                            <td scope="col"><small style="font-size: 10px;">MODALIDAD</small></td>
+                            <td scope="col"><small style="font-size: 10px;">TIPO DE CURSO</small></td>
+                            <td scope="col"><small style="font-size: 10px;">INSCRITO SICE</small></td>
+                            <td scope="col"><small style="font-size: 10px;">INSCRITO SIVYC<small></td>
+                            <td scope="col"><small style="font-size: 10px;">REALIZO SICE<small></td>
+                            <td scope="col"><small style="font-size: 10px;">REALIZO SIVYC<small></td>
                         </tr>
                     </thead>
                     <tbody>
                         @foreach ($data as $key=>$item)
                             <tr>
-                                <td scope="col" class="text-center"><small style="font-size: 8px;">{{$key}}</small></td>
-                                <td scope="col" class="text-center"><small style="font-size: 8px;">{{$item->unidad}}</small></td>
-                                <td scope="col" class="text-center"><small style="font-size: 8px;">{{$item->alumno}}</small></td>
-                                <td scope="col" class="text-center"><small style="font-size: 8px;">{{$item->matricula}}</small></td>
-                                <td scope="col" class="text-center"><small style="font-size: 8px;">{{$curp[$key]}}</td>
-                                <td scope="col" class="text-center"><small style="font-size: 8px;">{{$sexo[$key]}}</small></td>
-                                <td scope="col" class="text-center"><small style="font-size: 8px;">{{$item->espe}}</small></td>
-                                <td scope="col" class="text-center"><small style="font-size: 8px;">{{$item->curso}}</small></td>
-                                <td scope="col" class="text-center"><small style="font-size: 8px;">{{$item->clave}}</td>
-                                <td scope="col" class="text-center"><small style="font-size: 8px;">{{$item->mod}}</td>
-                                <td scope="col" class="text-center"><small style="font-size: 8px;">{{$item->tcapacitacion}}</td>
+                                <td scope="col" class="text-center"><small style="font-size: 10px;">{{$key}}</small></td>
+                                <td scope="col" class="text-center"><small style="font-size: 10px;">{{$item->unidad}}</small></td>
+                                <td scope="col" class="text-center"><small style="font-size: 10px;">{{$item->alumno}}</small></td>
+                                <td scope="col" class="text-center"><small style="font-size: 10px;">{{$item->matricula}}</small></td>
+                                <td scope="col" class="text-center"><small style="font-size: 10px;">{{$curp[$key]}}</td>
+                                <td scope="col" class="text-center"><small style="font-size: 10px;">{{$sexo[$key]}}</small></td>
+                                <td scope="col" class="text-center"><small style="font-size: 10px;">{{$item->espe}}</small></td>
+                                <td scope="col" class="text-center"><small style="font-size: 10px;">{{$item->curso}}</small></td>
+                                <td scope="col" class="text-center"><small style="font-size: 10px;">{{$item->clave}}</td>
+                                <td scope="col" class="text-center"><small style="font-size: 10px;">{{$item->mod}}</td>
+                                <td scope="col" class="text-center"><small style="font-size: 10px;">{{$item->tcapacitacion}}</td>
                                 @if ($item->realizo != NULL)
-                                    <td scope="col" class="text-center"><small style="font-size: 8px;">X</td>
+                                    <td scope="col" class="text-center"><small style="font-size: 10px;">X</td>
                                 @else
-                                    <td scope="col" class="text-center"><small style="font-size: 8px;"></td>
+                                    <td scope="col" class="text-center"><small style="font-size: 10px;"></td>
                                 @endif
                                 @if ($realizo[$key] != NULL)
-                                    <td scope="col" class="text-center"><small style="font-size: 8px;">X</td>
+                                    <td scope="col" class="text-center"><small style="font-size: 10px;">X</td>
                                 @else
-                                    <td scope="col" class="text-center"><small style="font-size: 8px;"></td>
+                                    <td scope="col" class="text-center"><small style="font-size: 10px;"></td>
                                 @endif
-                                <td scope="col" class="text-center"><small style="font-size: 8px;">{{$item->realizo}}</small></td>
-                                <td scope="col" class="text-center"><small style="font-size: 8px;">{{$realizo[$key]}}</small></td>
+                                <td scope="col" class="text-center"><small style="font-size: 10px;">{{$item->realizo}}</small></td>
+                                <td scope="col" class="text-center"><small style="font-size: 10px;">{{$realizo[$key]}}</small></td>
                             </tr>
                         @endforeach
                     </tbody>
