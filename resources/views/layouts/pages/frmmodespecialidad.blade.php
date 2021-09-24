e09ede4678b3b884c1aaa3245aca7e9dec9618d1
unknown <orlando@sidmac.com>
2021-09-24 14:22:55 -0500

    24092021 01 add select all & return back framodespecialidad

--- resources/views/layouts/pages/frmmodespecialidad.blade.php
+++ resources/views/layouts/pages/frmmodespecialidad.blade.php
@@ -238,6 +238,19 @@
 
                 <div class="card card-grid mb-4" role="grid" aria-labelledby="gridLabel">
                     <div class="card-header">
+                        <div class="form-row">
+                            <div class="form-group col-md-9"></div>
+                            <div class="form-group col-md-3">
+                                <input  type="checkbox" id="ckbCheckAll"
+                                    data-toggle="toggle"
+                                    data-style="ios"
+                                    data-on= "."
+                                    data-off= "."
+                                    data-onstyle="success"
+                                    data-offstyle="danger"
+                                    onchange="toggleOnOff()"/> Seleccionar Todo
+                            </div>
+                        </div>
                         <div class="row" role="row">
                             <div class="col-md-3" role="columnheader">
                                 <p class="form-control-plaintext">NOMBRE</p>
@@ -281,7 +294,7 @@
                                 <label>AÑADIR</label>
                                 <div class="form-control-plaintext text-truncate">
 
-                                    <input type="checkbox"
+                                    <input type="checkbox" class="checkBoxClass"
                                         @if($itemDataCatCurso->activo == TRUE)
                                             checked
                                         @endif
@@ -336,4 +349,14 @@
 @section('script_content_js')
     <script src="{{ asset("js/scripts/bootstrap-toggle.js") }}"></script>
     <script src="{{ asset("js/validate/orlandoValidate.js") }}"></script>
+    <script>
+        function toggleOnOff() {
+            var checkBox = document.getElementById("ckbCheckAll");
+            if (checkBox.checked == true){
+                $('.checkBoxClass').prop('checked', true).change()
+            } else {
+                $('.checkBoxClass').prop('checked', false).change()
+            }
+        }
+    </script>
 @endsection
