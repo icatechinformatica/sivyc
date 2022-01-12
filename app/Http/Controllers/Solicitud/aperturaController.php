e572150b024c60f382ed62aeedc870f89e2f8537
Armando Coutiño <armando97k@gmail.com>
2022-01-11 19:02:47 -0600

    modificacion aperturar

--- app/Http/Controllers/Solicitud/aperturaController.php
+++ app/Http/Controllers/Solicitud/aperturaController.php
@@ -103,7 +103,7 @@ class aperturaController extends Controller
                     $alumnos = DB::table('alumnos_registro as ar')->select('ar.id as id_reg','ap.curp','ap.nombre','ap.apellido_paterno','ap.apellido_materno','ap.fecha_nacimiento AS FN','ap.sexo AS SEX',
                     DB::raw("CONCAT(ap.apellido_paterno,' ', ap.apellido_materno,' ',ap.nombre) as alumno"),'ar.id_cerss', 'ap.lgbt',
                     'ap.estado_civil','ap.discapacidad','ap.nacionalidad','ap.etnia','ap.indigena','ap.inmigrante','ap.madre_soltera','ap.familia_migrante',
-                    'ar.costo','ar.tinscripcion',DB::raw("'0' as calificacion"),'ap.ultimo_grado_estudios as escolaridad','ap.empleado',
+                    'ar.costo','ar.tinscripcion',DB::raw("'0' as calificacion"),'ap.ultimo_grado_estudios as escolaridad','ap.empleado','ar.abrinscri',
                     'ap.matricula', 'ar.id_pre','ar.id', DB::raw("substring(curp,11,1) as sexo"),
                     DB::raw("substring(curp,5,2) as anio_nac"),
                     DB::raw("CASE WHEN substring(curp,5,2) <='".$anio_hoy."' THEN CONCAT('20',substring(curp,5,2),'-',substring(curp,7,2),'-',substring(curp,9,2))
@@ -406,7 +406,7 @@ class aperturaController extends Controller
                 $alumnos = $_SESSION['alumnos'];
                 foreach($alumnos as $a){
                     $tinscripcion = $a->tinscripcion;
-                    $abrinscriTMP = $abrinscri[$tinscripcion];
+                    $abrinscriTMP = $a->abrinscri;
                     $matricula = $a->matricula;
                     if(!$matricula AND $a->curp AND $grupo->cct){
                         $matricula = $this->genera_matricula($a->curp, $grupo->cct);
