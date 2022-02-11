5ea5c340ff8d435325480cd9edb0cee01278f0e9
Orlando Chávez <orlando@sidmac.com>
2022-02-11 08:58:00 -0600

    11022022 02

--- app/Http/Controllers/webController/ContratoController.php
+++ app/Http/Controllers/webController/ContratoController.php
@@ -825,7 +825,7 @@ class ContratoController extends Controller
         $testigo2 = directorio::WHERE('id', '=', $data_directorio->contrato_idtestigo2)->FIRST();
         $testigo3 = directorio::WHERE('id', '=', $data_directorio->contrato_idtestigo3)->FIRST();
 
-        $data = $contrato::SELECT('folios.id_folios','folios.importe_total','tbl_cursos.id','tbl_cursos.horas',
+        $data = $contrato::SELECT('folios.id_folios','folios.importe_total','tbl_cursos.id', 'tbl_cursos.clave','tbl_cursos.horas',
                                   'tbl_cursos.tipo_curso', 'tbl_cursos.clave','instructores.nombre','instructores.apellidoPaterno',
                                   'instructores.apellidoMaterno','instructores.folio_ine','instructores.rfc',
                                   'instructores.curp','instructores.domicilio')
