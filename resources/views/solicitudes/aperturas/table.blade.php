6610017f0229b86e403e6a6d6d19a8fd23c25297
Armandok97 <armando97k@gmail.com>
2022-07-25 12:22:39 -0500

    mod visualizacion soporte exoneracion en aperturas dta

--- resources/views/solicitudes/aperturas/table.blade.php
+++ resources/views/solicitudes/aperturas/table.blade.php
@@ -91,7 +91,7 @@
                                 <a class="nav-link" href="{{ $path.$g->comprobante_pago }}" target="_blank">
                                     <i class="fa fa-dollar-sign  fa-2x fa-lg text-primary" title="Comprobante de pago"></i>
                                 </a>
-                                @if ($g->mod=='CAE' AND $g->tipo!='PINS' AND $g->soporte_exo)
+                                @if ($g->soporte_exo)
                                     <a class="btn btn-danger btn-circle m-1 btn-circle-sm" data-toggle="tooltip"  target="_blank" data-placement="top" title="PDF EXONERACION"
                                         @if ($g->rev_exo) href="{{$path.$g->soporte_exo}}" @else href="{{$g->soporte_exo}}" @endif>
                                         <i class="fa fa-file-pdf-o" aria-hidden="true"></i>
