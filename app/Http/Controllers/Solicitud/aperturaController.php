58d055fc117664604377899687e875429983e3aa
Armandok97 <armando97k@gmail.com>
2022-11-29 15:06:50 -0600

    mod guardar ape

--- app/Http/Controllers/Solicitud/aperturaController.php
+++ app/Http/Controllers/Solicitud/aperturaController.php
@@ -368,7 +368,7 @@ class aperturaController extends Controller
                         } else {
                             $tipo_honorario = 'HONORARIOS';
                         }
-                        $exonerado = DB::table('exoneraciones')->where('folio_grupo', $grupo->folio_grupo)->where('status', '<>', null)->where('status', '<>', 'CANCELADO')->exists();
+                        $exonerado = DB::table('exoneraciones')->where('folio_grupo', $grupo->folio_grupo)->where('status', '<>', 'CAPTURA')->where('status', '<>', 'CANCELADO')->exists();
                         if ($request->hasFile('file_pago')) {
                             $file = $request->file_pago;
                             $tamanio = $file->getSize(); #obtener el tamaño del archivo del cliente
