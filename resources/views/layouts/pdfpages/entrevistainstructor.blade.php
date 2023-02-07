3b9749b09f9f9ee42e9919d70e554fb9dd19459e
Orlando Chávez <orlando@sidmac.com>
2023-02-07 10:05:55 -0600

    07022023 3.1

--- resources/views/layouts/pdfpages/entrevistainstructor.blade.php
+++ resources/views/layouts/pdfpages/entrevistainstructor.blade.php
@@ -14,7 +14,7 @@
                 /* margin: 10px; */
             }
             @page {
-                margin: 20px 30px 40px;
+                margin: 20px 30px 20px;
 
             }
             .ftr{
@@ -47,7 +47,7 @@
             footer {
             position: fixed;
             /* left: 0px; */
-            bottom: 40px;
+            bottom: 50px;
             /* right: 0px; */
             /* height: 60px; */
             /* text-align: center; */
@@ -120,7 +120,7 @@
             <div align=center> <b>Formato de Entrevista para Candidatos a Instructores</b></div>
             <div align=right> <b>{{$D}} de {{$M}} del {{$Y}}</b></div>
             <b>Nombre del entrevistado: {{$data->apellidoPaterno}} {{$data->apellidoMaterno}} {{$data->nombre}}</b>
-            <br><b>Unidad de capacitación: {{$userunidad->ubicacion}}
+            <br><b>Unidad de capacitación: {{$userunidad->ubicacion}}</b>
             <div class="table table-responsive">
                 <table class="tablad" style="border-color: black">
                     <thead>
@@ -189,8 +189,8 @@
                     </tbody>
                 </table>
             </div>
-            <div align=center><b>DECLARO BAJO PROTESTA DE DECIR VERDAD QUE LOS DATOS AQUÍ ASENTADOS SON CIERTOS</b></div>
-            <table class="table1" style="font-size:8; text-align:center;">
+            <div align=center style="-10px;"><b>DECLARO BAJO PROTESTA DE DECIR VERDAD QUE LOS DATOS AQUÍ ASENTADOS SON CIERTOS</b></div>
+            <table class="table1" style="font-size:8; text-align:center; margin-top: -10px;">
                 <tr>
                     <td colspan="2" width="360px"><p align="center"></p></td>
                     <td colspan="2" width="360px"><p align="center"></p></td>
@@ -202,8 +202,8 @@
                     <td></td>
                 </tr>
                 <tr>
-                    <td colspan="2">_________________________________________________</td>
-                    <td colspan="2">_________________________________________________</td>
+                    <td colspan="2"><b>_________________________________________________</b></td>
+                    <td colspan="2"><b>_________________________________________________</b></td>
                 </tr>
                 <tr>
                     <td colspan="2"><div align="center">{{$data->apellidoPaterno}} {{$data->apellidoMaterno}} {{$data->nombre}}</td></div>
