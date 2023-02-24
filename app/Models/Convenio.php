301324ccc54e7ee4661926092a6ec7d38595d604
Jose-Wicho <luisito08672@gmail.com>
2023-02-24 12:26:35 -0600

    Revert "mejoras convenio"
    This reverts commit 85459c490d05e169dc743703d6fac578562ed6fa.

--- app/Models/Convenio.php
+++ app/Models/Convenio.php
@@ -29,10 +29,7 @@ class Convenio extends Model
         return Carbon::parse($value)->format('d-m-Y');
     }
 
-    protected function scopeBusqueda($query, $tipo, $datos){
-        $buscar = $datos['campo_buscar'];
-        $fecha1 = $datos['fecha1'];
-        $fecha2 = $datos['fecha2'];
+    protected function scopeBusqueda($query, $tipo, $buscar){
         // dd($buscar);
         if (!empty($tipo)) {
             if (!empty(trim($buscar))) {
@@ -53,12 +50,7 @@ class Convenio extends Model
                         return $query->where('convenios.institucion', 'LIKE', "%$buscar%");
                         break;
                 }
-            }else if(!empty(trim($fecha1)) && !empty(trim($fecha2))){
-                return $query->whereBetween('convenios.updated_at', [$fecha1, $fecha2]);
             }
-
         }
-
     }
-
 }
