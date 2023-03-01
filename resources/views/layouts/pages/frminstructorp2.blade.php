cc7206c536437980d39b769c7ccfcb89aafb7e91
Icatech SIATA <icatech.gob.mx@gmail.com>
2023-03-01 11:51:18 -0600

    01032023 05

--- resources/views/layouts/pages/frminstructorp2.blade.php
+++ resources/views/layouts/pages/frminstructorp2.blade.php
@@ -67,7 +67,7 @@
                         <option value="sin especificar">Sin Especificar</option>
                         <option value="HONORARIOS" @if($datainstructor->tipo_honorario == 'HONORARIOS') selected @endif >Honorarios</option>
                         <option value="ASIMILADOS A SALARIO" @if($datainstructor->tipo_honorario == 'ASIMILADOS A SALARIO') selected @endif>Asimilados a Salarios</option>
-                        <option value="HONORARIOS Y ASIMILADOS A SALARIOS" @if($datainstructor->tipo_honorario == 'HONORARIOS Y ASIMILADOS A SALARIOS') selected @endif>Honorarios y Asimilado a Salarios</option>
+                        <option value="HONORARIOS Y ASIMILADOS A SALARIO" @if($datainstructor->tipo_honorario == 'HONORARIOS Y ASIMILADOS A SALARIO') selected @endif>Honorarios y Asimilado a Salarios</option>
                     </select>
                 </div>
             </div>
