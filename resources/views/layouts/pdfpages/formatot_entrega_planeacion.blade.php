0843d5d2e776c3fd4b8ae3e3c35fa474de0bd608
Jose-Wicho <luisito08672@gmail.com>
2023-03-03 15:20:13 -0600

    mejoras formato t

--- resources/views/layouts/pdfpages/formatot_entrega_planeacion.blade.php
+++ resources/views/layouts/pdfpages/formatot_entrega_planeacion.blade.php
@@ -116,8 +116,8 @@
             <div style="font-size:9px;"> <b>C.c.p  {{ $reg_unidad->dgeneral }}  , {{ $reg_unidad->pdgeneral }} . Para su conocimiento.</b> </div>
             <div style="font-size:9px"><b>C.c. {{ $directorio->nombre." ".$directorio->apellidoPaterno." ".$directorio->apellidoMaterno }} - {{ $directorio->puesto }} . Para su conocimiento.</b></div>
             <div style="font-size:9px;"> <b>Archivo: Minutario.</b> </div>
-            <div style="font-size:8px;"> <b>Validó:  {{ $jefeDepto->nombre." ".$jefeDepto->apellidoPaterno." ".$jefeDepto->apellidoMaterno }} - {{ $jefeDepto->puesto }} .</b> </div>
-            <div style="font-size:8px;"> <b>Elaboró:  AUXILIAR DEL DEPARTAMENTO DE CERTIFICACIÓN Y CONTROL.</b></div>
+            <div style="font-size:8px;"> <b>Validó: {{ $reg_unidad->dacademico }} - {{ $reg_unidad->pdacademico }}</b></div>
+            <div style="font-size:8px;"> <b>Elaboró: {{ $jefeDepto->nombre." ".$jefeDepto->apellidoPaterno." ".$jefeDepto->apellidoMaterno }} - {{ $jefeDepto->puesto }} . </b></div>
             {{-- <div style="font-size:8px;"> <b>Elaboró:  {{ $reg_unidad->academico }} .  {{ $reg_unidad->pacademico }} .</b></div> --}}
             <br><br>
         </div>
