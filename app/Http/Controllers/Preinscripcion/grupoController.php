aed388b830abc0f141f8fc2dcece3dc02d6288a5
rpnanguelu@gmail.com <rpnanguelu@gmail.com>
2023-06-27 13:24:53 -0600

    VALIDACION 150 INSTRUCTORES

--- app/Http/Controllers/Preinscripcion/grupoController.php
+++ app/Http/Controllers/Preinscripcion/grupoController.php
@@ -1328,7 +1328,7 @@ class grupoController extends Controller
                 COALESCE(
                     (select max(inicio) from tbl_cursos as c where c.id_instructor = $id_instructor
                         and COALESCE((select DATE_PART('day', tc.inicio::timestamp - c.termino::timestamp )
-                        from tbl_cursos as tc where tc.id_instructor = $id_instructor and tc.inicio>c.inicio order by tc.inicio ASC limit 1  )-1,0)>30 )
+                        from tbl_cursos as tc where tc.id_instructor = $id_instructor and tc.inicio>c.inicio order by tc.inicio ASC limit 1  )-1,0)>=30 )
                         , (select min(inicio)::timestamp - interval '1 day' from tbl_cursos where id_instructor = $id_instructor))
                 "))
                 ->value(DB::raw("DATE_PART('day', max(tc.termino)::timestamp - min(tc.inicio)::timestamp)+1"));
