35808d4ef09a42b7f98c2128a1626846b1d76cdf
rpnanguelu@gmail.com <rpnanguelu@gmail.com>
2023-07-05 15:09:51 -0600

    RANKING SOLICITUDES DE APOYO

--- resources/views/layouts/pages/home.blade.php
+++ resources/views/layouts/pages/home.blade.php
@@ -4,33 +4,35 @@
 @section('title', 'Inicio | Sivyc Icatech')
 
 @section('content')
-    <div class="card card-body" style=" min-height:450px;">
+    <link rel="stylesheet" href="{{asset('css/global.css') }}" />
+    <div class="card card-body">        
+        <h3 class="text-center text-muted font-weight-bold">RANKING</h2>
         <div class="row">
             <div class="form-group col-md-6">
                 {!! Form::open(['route' => 'home.post', 'method' => 'post', 'id'=>'frm', 'enctype' => 'multipart/form-data']) !!}
-                <h2 style="text-align:center;">RANKING (GESTION EXTEMPORANEA)</h2>
-                <div class="form-row mb-2">
-                    <div class="form-row col-md-3 ml-2">
-                        <label for="">MES:</label>
-                        {!! Form::select('mes', $meses, $mes, ['id'=>'mes','placeholder'=>'-- SELECCIONAR --','class'=>'form-control']) !!}
-                    </div>   
-                    <div class=" form-row col-md-3 ml-2">
-                        <label for="">EJERCICIO:</label>
-                        {!! Form::select('ejercicio', $ejercicios, $anio, ['id'=>'ejercicio','placeholder'=>'-- SELECCIONAR --','class'=>'form-control']) !!}
-                    </div>
-                    <div class="form-group col-md-1">
-                        {{ Form::button('FILTRAR', ['id'=>'buscar','class' => 'btn btn-primary']) }}
-                    </div>
+                <h4 class="text-center">GESTIÓN EXTEMPORÁNEA</h4><BR/>
+                <div class="form-row form-inline">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    {{ Form::select('mes', $meses, $mes ,['id'=>'mes','class' => 'form-control mr-sm-2','title' => 'MES','placeholder' => 'SELECCIONAR']) }}  
+                    {{ Form::select('ejercicio', $ejercicios, $anio, ['id'=>'ejercicio','class' => 'form-control mr-sm-2','title' => 'EJERCICIO','placeholder'=>'SELECCIONAR']) }}
+                    {{ Form::button('FILTRAR', ['id'=>'buscar','class' => 'btn']) }}                    
                 </div>
                 <div class="form-group">
                     <canvas id="myChart" width="600" height="300"></canvas>
                 </div>
                 {!! Form::close() !!}
             </div>
-            <div class="form-group col-md-6">
+            <div class="form-group col-md-6">                
+                <h4 class="text-center">SOLICITUDES DE ASISTENCIA TÉCNICA</h4><h5 class="text-center">JUNIO 2023</h5><BR/>
+                <div class="form-group">
+                    <canvas id="myChart2" width="600" height="320"></canvas>
+                </div>
             </div>
         </div>
     </div>
+    @php    
+        if($data=='[]') $data = '{"label":["CATAZAJA","COMITAN","JIQUIPILAS","OCOSINGO","REFORMA","SAN CRISTOBAL","TAPACHULA","TONALA","TUXTLA","VILLAFLORES","YAJALON"],"total":[0,0,0,0,0,0,0,0,0,0,0]}';
+    @endphp
+
     @section('script_content_js') 
         <script src="{{ asset('js/tablero/Chart.min.js') }}"></script>        	
         <script language="javascript"> 
@@ -44,19 +46,19 @@
                     data: {
                         labels: curso.label,
                         datasets: [{
-                            label:'TOTAL DE CURSOS',
+                            label:'CURSOS',
                             data: curso.total,
-                            backgroundColor: 'rgba(54, 162, 235, 0.5)'
+                            backgroundColor: 'rgba(176, 154, 91,0.7)'
                         },
                         {
-                            label:'GESTIONES EXTEMPORANEAS ARC01',
+                            label:'EXTEMPORANEIDAD ARC01',
                             data: curso.ex1,
-                            backgroundColor: 'rgba(255, 99, 132, 0.5)'
+                            backgroundColor: 'rgba(98,17,50, 0.5)'
                         },
                         {
-                            label:'GESTIONES EXTEMPORANEAS ARC02',
+                            label:'EXTEMPORANEIDAD ARC02',
                             data: curso.ex2,
-                            backgroundColor: 'rgba(153, 102, 255, 0.5)'
+                            backgroundColor: 'rgba(51,51,51, 0.5)'
                         }]
                     },
                     options: {
@@ -99,7 +101,66 @@
                         }
                     }
                 });  
+               
+                /*RANKING SOLICITUDES DE APOYO O ASISTENCIA TÉCNICA*/
+                var ctx = document.getElementById('myChart2').getContext('2d');
+                var curso = JSON.parse('{"label":["CATAZAJA","COMITAN","JIQUIPILAS","OCOSINGO","REFORMA","SAN CRISTOBAL","TAPACHULA","TONALA","TUXTLA","VILLAFLORES","YAJALON"],"total":[6,6,1,7,0,3,3,2,13,2,7]}'); 
+                var myChart = new Chart(ctx, {
+                    type: 'bar',
+                    data: {
+                        labels: curso.label,
+                        datasets: [{
+                            label:'SOLICITUDES',
+                            data: curso.total,
+                            backgroundColor: 'rgba(176, 154, 91, 0.7)'
+                        },
+                        ]
+                    },
+                    options: {
+                        scales:{
+                            yAxes:[{
+                                ticks:{
+                                    beginAtZero: true
+                                }
+                            }]
+                        },
+                        "hover": {
+                            "animationDuration": 0
+                        },
+                        "animation": {
+                            "duration": 1,
+                            "onComplete": function() {
+                                var chartInstance = this.chart,
+                                    ctx = chartInstance.ctx;
+    
+                                ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart
+                                    .defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
+                                ctx.textAlign = 'center';
+                                ctx.textBaseline = 'bottom';
+    
+                                this.data.datasets.forEach(function(dataset, i) {
+                                    var meta = chartInstance.controller.getDatasetMeta(i);
+                                    meta.data.forEach(function(bar, index) {
+                                        var data = dataset.data[index];
+                                        ctx.fillText(data, bar._model.x, bar._model.y);
+                                    });
+                                });
+    
+                            }
+                        },
+                        legend: {
+                            "display": true
+                        },
+                        tooltips: {
+                            "enabled": false
+                        }
+                    }
+                });  
+
             });
+
+            
+
         </script>
     @endsection
 @endsection
