19bd57532eb01a72a0bd579d9ced2845174e4c38
rpnanguelu@gmail.com <rpnanguelu@gmail.com>
2023-08-30 14:23:56 -0600

    RUOTES  LETRA  MAYUS RECIBOS Y TRANSFERENCIA

--- routes/solicitudes/routes_transferencia.php
+++ routes/solicitudes/routes_transferencia.php
@@ -2,11 +2,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth'])->group(function(){    
-    Route::get('/solicitudes/transferencia/index', 'solicitudes\transferenciaController@index')->name('solicitudes.transferencia.index')->middleware('can:solicitudes.transferencia');
-    Route::post('/solicitudes/transferencia/index', 'solicitudes\transferenciaController@index')->name('solicitudes.transferencia.index')->middleware('can:solicitudes.transferencia');
-    Route::get('/solicitudes/transferencia/marcar', 'solicitudes\transferenciaController@marcar')->name('solicitudes.transferencia.marcar')->middleware('can:solicitudes.transferencia');
-    Route::post('/solicitudes/transferencia/marcar', 'solicitudes\transferenciaController@marcar')->name('solicitudes.transferencia.marcar')->middleware('can:solicitudes.transferencia');
-    Route::post('/solicitudes/transferencia/generar', 'solicitudes\transferenciaController@generar')->name('solicitudes.transferencia.generar')->middleware('can:solicitudes.transferencia');
-    Route::get('/solicitudes/transferencia/deshacer', 'solicitudes\transferenciaController@deshacer')->name('solicitudes.transferencia.deshacer')->middleware('can:solicitudes.deshacer');
-    Route::post('/solicitudes/transferencia/deshacer', 'solicitudes\transferenciaController@deshacer')->name('solicitudes.transferencia.deshacer')->middleware('can:solicitudes.deshacer');
+    Route::get('/solicitudes/transferencia/index', 'Solicitudes\transferenciaController@index')->name('solicitudes.transferencia.index')->middleware('can:solicitudes.transferencia');
+    Route::post('/solicitudes/transferencia/index', 'Solicitudes\transferenciaController@index')->name('solicitudes.transferencia.index')->middleware('can:solicitudes.transferencia');
+    Route::get('/solicitudes/transferencia/marcar', 'Solicitudes\transferenciaController@marcar')->name('solicitudes.transferencia.marcar')->middleware('can:solicitudes.transferencia');
+    Route::post('/solicitudes/transferencia/marcar', 'Solicitudes\transferenciaController@marcar')->name('solicitudes.transferencia.marcar')->middleware('can:solicitudes.transferencia');
+    Route::post('/solicitudes/transferencia/generar', 'Solicitudes\transferenciaController@generar')->name('solicitudes.transferencia.generar')->middleware('can:solicitudes.transferencia');
+    Route::get('/solicitudes/transferencia/deshacer', 'Solicitudes\transferenciaController@deshacer')->name('solicitudes.transferencia.deshacer')->middleware('can:solicitudes.deshacer');
+    Route::post('/solicitudes/transferencia/deshacer', 'Solicitudes\transferenciaController@deshacer')->name('solicitudes.transferencia.deshacer')->middleware('can:solicitudes.deshacer');
 });
