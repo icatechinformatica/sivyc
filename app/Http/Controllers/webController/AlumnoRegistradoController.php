dc29d1ce11a40c34286119690d3b93b7da43e857
Orlando Chavez <orlando@sidmac.com>
2024-01-17 10:08:49 -0600

    17012024 01 upd fecha de sid

--- app/Http/Controllers/webController/AlumnoRegistradoController.php
+++ app/Http/Controllers/webController/AlumnoRegistradoController.php
@@ -198,7 +198,7 @@ class AlumnoRegistradoController extends Controller
                             ->LEFTJOIN('alumnos_pre', 'alumnos_pre.id', '=', 'alumnos_registro.id_pre')
                             ->LEFTJOIN('tbl_unidades', 'alumnos_registro.unidad', '=', 'tbl_unidades.unidad')
                             ->FIRST(['alumnos_registro.nombre AS nombrealumno', 'alumnos_registro.apellido_paterno', 'alumnos_registro.apellido_materno', 'alumnos_pre.correo', 'alumnos_pre.telefono','alumnos_pre.telefono_casa','alumnos_pre.telefono_personal',
-                            'alumnos_registro.curp AS curp_alumno', 'alumnos_registro.folio_grupo', 'alumnos_pre.sexo','alumnos_pre.chk_acta_nacimiento','alumnos_pre.chk_curp','alumnos_pre.chk_comprobante_domicilio','alumnos_pre.chk_fotografia',
+                            'alumnos_registro.curp AS curp_alumno', 'alumnos_registro.inicio', 'alumnos_registro.folio_grupo', 'alumnos_pre.sexo','alumnos_pre.chk_acta_nacimiento','alumnos_pre.chk_curp','alumnos_pre.chk_comprobante_domicilio','alumnos_pre.chk_fotografia',
                             'alumnos_pre.fecha_nacimiento', 'alumnos_pre.correo','alumnos_pre.domicilio','alumnos_pre.fotografia', 'alumnos_pre.colonia', 'alumnos_pre.cp', 'alumnos_pre.municipio','alumnos_pre.chk_ine','alumnos_pre.chk_pasaporte_licencia',
                             'alumnos_pre.chk_comprobante_ultimo_grado','alumnos_registro.escolaridad AS ultimo_grado_estudios','alumnos_pre.chk_comprobante_calidad_migratoria','alumnos_pre.estado', 'alumnos_pre.estado_civil', 'alumnos_pre.discapacidad', 'alumnos_registro.no_control', 'alumnos_registro.id',
                             'alumnos_registro.horario', 'alumnos_registro.grupo', 'alumnos_registro.tipo_curso AS tipocurso', 'alumnos_pre.empresa_trabaja', 'alumnos_pre.puesto_empresa', 'alumnos_pre.antiguedad','alumnos_pre.empleado',
@@ -206,7 +206,9 @@ class AlumnoRegistradoController extends Controller
                             'cursos.nombre_curso', 'especialidades.nombre AS especialidad', 'tbl_unidades.cct AS unidades', 'alumnos_registro.cerrs','cursos.tipo_curso',
                             'alumnos_registro.etnia', 'alumnos_registro.fecha', 'alumnos_pre.medio_entero', 'alumnos_pre.sistema_capacitacion_especificar', 'alumnos_registro.realizo', 'cursos.costo']);
         $edad = Carbon::parse($alumnos->fecha_nacimiento)->age; //dd($alumnos);
-        $date = carbon::now()->toDateString();
+        $date = Carbon::now()->toDateString();
+        $fechaCarbon = Carbon::parse($alumnos->inicio);
+        $alumnos->inicio = $fechaCarbon->format('d-m-Y');
         set_time_limit(300);
 
         // Descomentar este pathimg si se trabajara con el archivo de forma local
