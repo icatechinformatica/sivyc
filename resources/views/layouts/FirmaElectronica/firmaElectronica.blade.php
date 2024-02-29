7ac1a98beb98638088e0de4a894a984ddc573120
Orlando Chavez <orlando@sidmac.com>
2024-02-29 15:02:04 -0600

    290202024 01 fix cancelaciom y sellado

--- resources/views/layouts/FirmaElectronica/firmaElectronica.blade.php
+++ resources/views/layouts/FirmaElectronica/firmaElectronica.blade.php
@@ -352,7 +352,9 @@
                                                             <td>
                                                                 {{-- @if ($obj['emisor']['_attributes']['email'] == $email) --}}
                                                                 @can('efirma.sellar')
-                                                                    @if ($sendValidation)
+                                                                    @if ($sendValidation && $docFirmado->tipo_archivo == 'Contrato' && $rol->role_id == '2')
+                                                                        <button type="button" onclick="sellardocumento('{{$docFirmado->id}}')" class="btn btn-outline-primary">Sellar</button>
+                                                                    @elseif($sendValidation && in_array($docFirmado->tipo_archivo, ['Lista de asistencia','Lista de calificaciones','Reporte fotografico']) && $rol->role_id == '30')
                                                                         <button type="button" onclick="sellardocumento('{{$docFirmado->id}}')" class="btn btn-outline-primary">Sellar</button>
                                                                     @else
                                                                         Faltan Firmas
