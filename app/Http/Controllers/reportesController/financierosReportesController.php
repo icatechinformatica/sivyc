e06ef55bc3fa48c101f3ed7d9c104a3750398e7d
Orlando Chavez <orlando@sidmac.com>
2024-04-02 13:50:00 -0600

    02042024 upd excel financieros

--- app/Http/Controllers/reportesController/financierosReportesController.php
+++ app/Http/Controllers/reportesController/financierosReportesController.php
@@ -33,7 +33,7 @@ class financierosReportesController extends Controller
 
     public function cursos_xls(Request $request) {
 
-        $query = DB::Table('tbl_cursos')->Select('tbl_cursos.clave','tbl_cursos.dura',"instructores.apellidoPaterno","instructores.apellidoMaterno",
+        $query = DB::Table('tbl_cursos')->Select('tbl_cursos.clave','tbl_cursos.dura','tbl_cursos.soportes_instructor','tbl_cursos.curso',"instructores.apellidoPaterno","instructores.apellidoMaterno",
             'instructores.nombre','tbl_cursos.rfc','tbl_cursos.curp','tbl_cursos.inicio','tbl_cursos.termino','folios.folio_validacion','folios.iva',
             'folios.importe_total','contratos.arch_factura_xml')
             ->Join('tbl_unidades','tbl_unidades.unidad','tbl_cursos.unidad')
@@ -60,9 +60,9 @@ class financierosReportesController extends Controller
         }
 
         $data = $query->Get();
-        // dd($data);
+        // dd(json_decode($data[0]->soportes_instructor));
 
-        $head = ['CLAVE','HORAS CURSO','INSTRUCTOR','SUFICIENCIA PRESUPUESTAL','URL FACTURA XML'];
+        $head = ['CLAVE','HORAS CURSO','CURSO','INSTRUCTOR','BANCO','NO. CUENTA','CLABE','SUFICIENCIA PRESUPUESTAL','URL FACTURA XML'];
         $title = "Cursos Reporte " . $request->status;
         $name = $title."_".date('Ymd').".xlsx";
         $view = 'layouts.pages.reportes.financieros.xls_reporte_cursos';
--- resources/views/layouts/pages/reportes/financieros/xls_reporte_cursos.blade.php
+++ resources/views/layouts/pages/reportes/financieros/xls_reporte_cursos.blade.php
@@ -5,11 +5,15 @@
             <th><b>CONS.</b></th>
             <th><b>CLAVE</b></th>
             <th><b>HORAS CURSO</b></th>
+            <th><b>CURSO</b></th>
             <th><b>NOMBRE</b></th>
             <th><b>APELLIDO PATERNO</b></th>
             <th><b>APELLIDO MATERNO</b></th>
             <th><b>RFC</b></th>
             <th><b>CURP</b></th>
+            <th><b>BANCO</b></th>
+            <th><b>NO. CUENTA</b></th>
+            <th><b>CLABE</b></th>
             <th><b>SUFICIENCIA PRESUPUESTAL</b></th>
             <th><b>FECHA CONTRATO</b></th>
             <th><b>MONTO CONTRATO</b></th>
@@ -22,15 +26,23 @@
         @php $total = null;@endphp
         @foreach ($data as $key => $cadwell)
             <tr>
-                @php $total = $cadwell->importe_total - $cadwell->iva; @endphp
+                @php
+                    $total = $cadwell->importe_total - $cadwell->iva;
+                    $datosBanco = json_decode($cadwell->soportes_instructor);
+                @endphp
+
                 <td>{{$key+1}}</td>
                 <td>{{$cadwell->clave}}</td>
                 <td>{{$cadwell->dura}}</td>
+                <td>{{$cadwell->curso}}</td>
                 <td>{{$cadwell->nombre}}</td>
                 <td>{{$cadwell->apellidoPaterno}}</td>
                 <td>{{$cadwell->apellidoMaterno}}</td>
                 <td>{{$cadwell->rfc}}</td>
                 <td>{{$cadwell->curp}}</td>
+                <td>{{$datosBanco->banco}}</td>
+                <td>{{$datosBanco->no_cuenta}}</td>
+                <td>{{$datosBanco->interbancaria}}</td>
                 <td>{{$cadwell->folio_validacion}}</td>
                 <td>{{$cadwell->inicio}}</td>
                 <td>{{$total}}</td>
