47c5e3dd52b67b38ee23eabe8d7d055b56ec82ff
Orlando Chavez <orlando@sidmac.com>
2024-04-09 15:34:15 -0600

    09042024 01 fi clean hvalidacion vista

--- resources/views/layouts/pages/verinstructor.blade.php
+++ resources/views/layouts/pages/verinstructor.blade.php
@@ -3090,6 +3090,7 @@
             option,
             i = 0,
             il = id.length;
+            selectL.empty();
             // console.log(il);
             // console.log( id[0])
             for (; i < il; i += 1)
