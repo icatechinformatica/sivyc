543aca083c62077f3e27bc84c8e87eb543b30a84
Orlando Chavez <orlando@sidmac.com>
2024-05-02 11:58:30 -0600

    02052024 02

--- app/Http/Controllers/efirma/AsistenciaController.php
+++ app/Http/Controllers/efirma/AsistenciaController.php
@@ -43,10 +43,10 @@ class AsistenciaController extends Controller
         'org.nombre', 'fun.incapacidad', 'fun.id as id_fun')
                             ->Join('tbl_funcionarios AS fun','fun.id','org.id')
                             ->Where('org.id', Auth::user()->id_organismo)
-                            ->Where('org.nombre', 'LIKE', 'DEPARTAMENTO ACADEMICO%')
+                            ->Where('org.nombre', 'LIKE', 'DEPARTAMENTO ACADÉMICO%')
                             ->OrWhere('org.id_parent', Auth::user()->id_organismo)
                             // ->Where('org.nombre', 'NOT LIKE', 'CENTRO%')
-                            ->Where('org.nombre', 'LIKE', 'DEPARTAMENTO ACADEMICO%')
+                            ->Where('org.nombre', 'LIKE', 'DEPARTAMENTO ACADÉMICO%')
                             ->First();
         if($dataFirmante == null){
             return redirect()->route('firma.inicio')->with('danger', 'NO SE ENCONTRARON DATOS DEL FIRMANTE AL REALIZAR LA CONSULTA');
