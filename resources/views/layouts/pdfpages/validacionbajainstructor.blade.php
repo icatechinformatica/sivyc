fbcc3219ddcd8d0813f1a2971102d19f0822fcc8
Orlando Chavez <orlando@sidmac.com>
2024-05-03 15:21:34 -0600

    03052024 fix documentos formato T y baja de instructor

--- resources/views/layouts/pdfpages/validacionbajainstructor.blade.php
+++ resources/views/layouts/pdfpages/validacionbajainstructor.blade.php
@@ -124,7 +124,7 @@
             <p class='direccion'><b>@foreach($direccion as $point => $ari)@if($point != 0)<br> @endif {{$ari}}@endforeach</b></p>
         </footer>
         <div>
-            <div align=right> <b>Unidad de Capacitación {{$data_unidad->ubicacion}}</b> </div>
+            <div align=right> <b>Dirección Técnica Académica</b> </div>
             <div align=right> <b>Memorandum No. {{$especialidades[0]->memorandum_baja}}</b></div>
             <div align=right> <b>{{$especialidades[0]->unidad_solicita}}, Chiapas {{$D}} de {{$M}} del {{$Y}}.</b></div>
 
