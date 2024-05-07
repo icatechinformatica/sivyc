6c3b9b64e379ad412771e8e64192db9dddbb5545
Jose-Wicho <luisito08672@gmail.com>
2024-05-07 12:51:59 -0600

    Margen del pagina soporte constancias

--- resources/views/theme/formatos/vlayout.blade.php
+++ resources/views/theme/formatos/vlayout.blade.php
@@ -15,7 +15,7 @@
     }
     @page {
         /* margin: 110px 20px 53px; */
-        margin: 120px 30px 40px 30px;
+        margin: 120px 30px 50px 30px;
     }
     header {
         position: fixed;
