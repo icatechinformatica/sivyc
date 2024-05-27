01af3b6bfff82aaf36be72283bef908d08603429
Orlando Chavez <orlando@sidmac.com>
2024-05-27 11:22:38 -0600

    27052024 01

--- app/Http/Controllers/webController/InstructorController.php
+++ app/Http/Controllers/webController/InstructorController.php
@@ -4446,6 +4446,7 @@ class InstructorController extends Controller
         ->where(function ($query) {
             $query->whereNotIn('pagos.status_recepcion', ['VALIDADO', 'recepcion tradicional'])
                 ->orWhereNull('pagos.status_recepcion')
+                ->orWhere('pagos.status', 'Rechazado')
                 ->orWhere('folios.edicion_pago','TRUE');
         })
         ->Get();
