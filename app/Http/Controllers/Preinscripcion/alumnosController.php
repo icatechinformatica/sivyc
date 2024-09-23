8e56abdff39fa52fc2b7a47e0492f83f43f4e989
rpnanguelu@gmail.com <rpnanguelu@gmail.com>
2024-09-23 11:43:41 -0600

    MEJORAS BUSQUEDA ALUMNOS02

--- app/Http/Controllers/Preinscripcion/alumnosController.php
+++ app/Http/Controllers/Preinscripcion/alumnosController.php
@@ -1,23 +1,9 @@
 <?php
-
 namespace App\Http\Controllers\Preinscripcion;
 use Illuminate\Support\Facades\Auth;
-
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Alumno;
-use App\Models\Alumnopre;
-use Carbon\Carbon;
-use App\Models\Unidad;
-use App\Models\Municipio;
-use App\Models\Estado;
-use App\Models\especialidad;
-use App\Models\curso;
-use App\Models\tbl_unidades;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\DB;
-// reference the Dompdf namespace
-use PDF;
 
 class alumnosController extends Controller
 {
--- routes/preinscripcion/routes_alumnos.php
+++ routes/preinscripcion/routes_alumnos.php
@@ -1,13 +1,6 @@
 <?php
 use Illuminate\Support\Facades\Route;
 Route::middleware(['auth'])->group(function(){        
-    Route::get('/preinscripcion/alumnos', 'preinscripcion\alumnosController@index')->name('preinscripcion.alumnos')
+    Route::get('/preinscripcion/alumnos', 'Preinscripcion\alumnosController@index')->name('preinscripcion.alumnos')
     ->middleware('can:preinscripcion.alumnos');   
 });
-
-/*
-    Route::get('alumnos/registrados/{id}', 'webController\AlumnoRegistradoController@show')->name('alumnos.inscritos.detail')
-    ->middleware('can:alumno.inscrito.show');
-    Route::get('alumnos/registrados', 'webController\AlumnoRegistradoController@index')->name('alumnos.inscritos')
-    ->middleware('can:alumnos.inscritos.index');
-*/
