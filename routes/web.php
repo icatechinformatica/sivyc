49b26bba02d34a3dd55c53345d5f7a2ac6fcb7cd
Orlando Chavez <orlando@sidmac.com>
2024-10-03 09:47:44 -0600

    03102024 01

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('/pago/historial/Validado/{id}', 'webController\PagoController@histor
 Route::get('/contrato/historial/validado/{id}', 'webController\ContratoController@historial_validado')->name('contrato-validado-historial');
 Route::get('/contrato/eliminar/{id}', 'webController\ContratoController@delete')->name('eliminar-contrato');
 Route::get('/contrato/previsualizacion/{id}', 'webController\ContratoController@pre_contratoPDF')->name('pre_contrato');
-Route::get('/prueba', 'webController\InstructorController@prueba');
+Route::get('/prueba', 'webController\PagoController@prueba');
 Route::get('/prueba2', 'webController\CursosController@prueba2');
 Route::get('/contrato/reiniciar/{id}', 'webController\ContratoController@contractRestart')->name('reiniciar-contrato');
 
