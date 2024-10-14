842a5d8330c14303541bed39bb9b5eb525a24a26
Orlando Chavez <orlando@sidmac.com>
2024-10-14 15:43:06 -0600

    14102024 01 minifix titular true en epagocontroller

--- app/Http/Controllers/efirma/EPagoController.php
+++ app/Http/Controllers/efirma/EPagoController.php
@@ -51,6 +51,7 @@ class EPagoController extends Controller
             ->Join('tbl_unidades AS u', 'u.id', 'org.id_unidad')
             ->Where('org.id_parent',1)
             ->Where('fun.activo', 'true')
+            ->Where('fun.titular', true)
             ->Where('u.unidad', $info->ubicacion)
             ->First();
 
