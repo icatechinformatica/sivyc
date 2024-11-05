71ba5fdf53b09c19b99b411708707d32113c000d
Orlando Chavez <orlando@sidmac.com>
2024-11-05 12:46:06 -0600

    05112024 01 mejora de calificaciones

--- app/Http/Controllers/efirma/AsistenciaController.php
+++ app/Http/Controllers/efirma/AsistenciaController.php
@@ -23,153 +23,154 @@ class AsistenciaController extends Controller
         $this->mes = ["01" => "ENERO", "02" => "FEBRERO", "03" => "MARZO", "04" => "ABRIL", "05" => "MAYO", "06" => "JUNIO", "07" => "JULIO", "08" => "AGOSTO", "09" => "SEPTIEMBRE", "10" => "OCTUBRE", "11" => "NOVIEMBRE", "12" => "DICIEMBRE"];
     }
 
-    public function generar_xml(Request $request) {
-        $info = DB::Table('tbl_cursos')->Select('tbl_unidades.*','tbl_cursos.clave','tbl_cursos.nombre','tbl_cursos.curp','instructores.correo')
-                ->Join('tbl_unidades','tbl_unidades.unidad','tbl_cursos.unidad')
-                ->join('instructores','instructores.id','tbl_cursos.id_instructor')
-                ->Where('tbl_cursos.id',$request->txtIdValidado)
-                ->First();
+    // ELIMINAR DESPUES DE 01/01/2025
+    // public function generar_xml(Request $request) {
+    //     $info = DB::Table('tbl_cursos')->Select('tbl_unidades.*','tbl_cursos.clave','tbl_cursos.nombre','tbl_cursos.curp','instructores.correo')
+    //             ->Join('tbl_unidades','tbl_unidades.unidad','tbl_cursos.unidad')
+    //             ->join('instructores','instructores.id','tbl_cursos.id_instructor')
+    //             ->Where('tbl_cursos.id',$request->txtIdValidado)
+    //             ->First();
 
-        $body = $this->create_body($request->txtIdValidado,$info); //creacion de body
-        $body = str_replace(["\r", "\n", "\f"], ' ', $body);
+    //     $body = $this->create_body($request->txtIdValidado,$info); //creacion de body
+    //     $body = str_replace(["\r", "\n", "\f"], ' ', $body);
 
-        $nameFileOriginal = 'Lista de asistencia '.$info->clave.'.pdf';
-        $numOficio = 'LAD-04-'.$info->clave;
-        $numFirmantes = '2';
+    //     $nameFileOriginal = 'Lista de asistencia '.$info->clave.'.pdf';
+    //     $numOficio = 'LAD-04-'.$info->clave;
+    //     $numFirmantes = '2';
 
-        $arrayFirmantes = [];
+    //     $arrayFirmantes = [];
 
-        $dataFirmante = DB::Table('tbl_organismos AS org')->Select('org.id','fun.nombre AS funcionario','fun.curp','fun.cargo','fun.correo',
-        'org.nombre', 'fun.incapacidad', 'fun.id as id_fun')
-                            ->Join('tbl_funcionarios AS fun','fun.id','org.id')
-                            ->Where('org.id', Auth::user()->id_organismo)
-                            ->Where('fun.titular', true)
-                            ->Where('org.nombre', 'LIKE', 'DEPARTAMENTO ACADÉMICO%')
-                            ->OrWhere('org.id_parent', Auth::user()->id_organismo)
-                            // ->Where('org.nombre', 'NOT LIKE', 'CENTRO%')
-                            ->Where('org.nombre', 'LIKE', 'DEPARTAMENTO ACADÉMICO%')
-                            ->First();
-        if($dataFirmante == null){
-            return redirect()->route('firma.inicio')->with('danger', 'NO SE ENCONTRARON DATOS DEL FIRMANTE AL REALIZAR LA CONSULTA');
-        }
-        ##Incapacidad
-        $val_inca = $this->valid_incapacidad($dataFirmante);
-        if ($val_inca != null) {
-            $dataFirmante = $val_inca;
-        }
+    //     $dataFirmante = DB::Table('tbl_organismos AS org')->Select('org.id','fun.nombre AS funcionario','fun.curp','fun.cargo','fun.correo',
+    //     'org.nombre', 'fun.incapacidad', 'fun.id as id_fun')
+    //                         ->Join('tbl_funcionarios AS fun','fun.id','org.id')
+    //                         ->Where('org.id', Auth::user()->id_organismo)
+    //                         ->Where('fun.titular', true)
+    //                         ->Where('org.nombre', 'LIKE', 'DEPARTAMENTO ACADÉMICO%')
+    //                         ->OrWhere('org.id_parent', Auth::user()->id_organismo)
+    //                         // ->Where('org.nombre', 'NOT LIKE', 'CENTRO%')
+    //                         ->Where('org.nombre', 'LIKE', 'DEPARTAMENTO ACADÉMICO%')
+    //                         ->First();
+    //     if($dataFirmante == null){
+    //         return redirect()->route('firma.inicio')->with('danger', 'NO SE ENCONTRARON DATOS DEL FIRMANTE AL REALIZAR LA CONSULTA');
+    //     }
+    //     ##Incapacidad
+    //     $val_inca = $this->valid_incapacidad($dataFirmante);
+    //     if ($val_inca != null) {
+    //         $dataFirmante = $val_inca;
+    //     }
 
-        //Llenado de funcionarios firmantes
-        $temp = ['_attributes' =>
-            [
-                'curp_firmante' => $info->curp,
-                'nombre_firmante' => $info->nombre,
-                'email_firmante' => $info->correo,
-                'tipo_firmante' => 'FM'
-            ]
-        ];
-        array_push($arrayFirmantes, $temp);
+    //     //Llenado de funcionarios firmantes
+    //     $temp = ['_attributes' =>
+    //         [
+    //             'curp_firmante' => $info->curp,
+    //             'nombre_firmante' => $info->nombre,
+    //             'email_firmante' => $info->correo,
+    //             'tipo_firmante' => 'FM'
+    //         ]
+    //     ];
+    //     array_push($arrayFirmantes, $temp);
 
-        $temp = ['_attributes' =>
-            [
-                'curp_firmante' => $dataFirmante->curp,
-                'nombre_firmante' => $dataFirmante->funcionario,
-                'email_firmante' => $dataFirmante->correo,
-                'tipo_firmante' => 'FM'
-            ]
-        ];
-        array_push($arrayFirmantes, $temp);
+    //     $temp = ['_attributes' =>
+    //         [
+    //             'curp_firmante' => $dataFirmante->curp,
+    //             'nombre_firmante' => $dataFirmante->funcionario,
+    //             'email_firmante' => $dataFirmante->correo,
+    //             'tipo_firmante' => 'FM'
+    //         ]
+    //     ];
+    //     array_push($arrayFirmantes, $temp);
 
-        $ArrayXml = [
-            'emisor' => [
-                '_attributes' => [
-                    'nombre_emisor' => Auth::user()->name,
-                    'cargo_emisor' => Auth::user()->puesto,
-                    'dependencia_emisor' => 'Instituto de Capacitación y Vinculación Tecnológica del Estado de Chiapas'
-                    // 'curp_emisor' => $dataEmisor->curp
-                ],
-            ],
-            'archivo' => [
-                '_attributes' => [
-                    'nombre_archivo' => $nameFileOriginal
-                    // 'md5_archivo' => $md5
-                    // 'checksum_archivo' => utf8_encode($text)
-                ],
-                // 'cuerpo' => ['Por medio de la presente me permito solicitar el archivo '.$nameFile]
-                'cuerpo' => [$body]
-            ],
-            'firmantes' => [
-                '_attributes' => [
-                    'num_firmantes' => $numFirmantes
-                ],
-                'firmante' => [
-                    $arrayFirmantes
-                ]
-            ],
-        ];
-        //Creacion de estampa de hora exacta de creacion
-        $date = Carbon::now();
-        $month = $date->month < 10 ? '0'.$date->month : $date->month;
-        $day = $date->day < 10 ? '0'.$date->day : $date->day;
-        $hour = $date->hour < 10 ? '0'.$date->hour : $date->hour;
-        $minute = $date->minute < 10 ? '0'.$date->minute : $date->minute;
-        $second = $date->second < 10 ? '0'.$date->second : $date->second;
-        $dateFormat = $date->year.'-'.$month.'-'.$day.'T'.$hour.':'.$minute.':'.$second;
+    //     $ArrayXml = [
+    //         'emisor' => [
+    //             '_attributes' => [
+    //                 'nombre_emisor' => Auth::user()->name,
+    //                 'cargo_emisor' => Auth::user()->puesto,
+    //                 'dependencia_emisor' => 'Instituto de Capacitación y Vinculación Tecnológica del Estado de Chiapas'
+    //                 // 'curp_emisor' => $dataEmisor->curp
+    //             ],
+    //         ],
+    //         'archivo' => [
+    //             '_attributes' => [
+    //                 'nombre_archivo' => $nameFileOriginal
+    //                 // 'md5_archivo' => $md5
+    //                 // 'checksum_archivo' => utf8_encode($text)
+    //             ],
+    //             // 'cuerpo' => ['Por medio de la presente me permito solicitar el archivo '.$nameFile]
+    //             'cuerpo' => [$body]
+    //         ],
+    //         'firmantes' => [
+    //             '_attributes' => [
+    //                 'num_firmantes' => $numFirmantes
+    //             ],
+    //             'firmante' => [
+    //                 $arrayFirmantes
+    //             ]
+    //         ],
+    //     ];
+    //     //Creacion de estampa de hora exacta de creacion
+    //     $date = Carbon::now();
+    //     $month = $date->month < 10 ? '0'.$date->month : $date->month;
+    //     $day = $date->day < 10 ? '0'.$date->day : $date->day;
+    //     $hour = $date->hour < 10 ? '0'.$date->hour : $date->hour;
+    //     $minute = $date->minute < 10 ? '0'.$date->minute : $date->minute;
+    //     $second = $date->second < 10 ? '0'.$date->second : $date->second;
+    //     $dateFormat = $date->year.'-'.$month.'-'.$day.'T'.$hour.':'.$minute.':'.$second;
 
-        $result = ArrayToXml::convert($ArrayXml, [
-            'rootElementName' => 'DocumentoChis',
-            '_attributes' => [
-                'version' => '2.0',
-                'fecha_creacion' => $dateFormat,
-                'no_oficio' => $numOficio,
-                'dependencia_origen' => 'Instituto de Capacitación y Vinculación Tecnológica del Estado de Chiapas',
-                'asunto_docto' => 'Lista de asistencia LAD-04',
-                'tipo_docto' => 'OFC',
-                'xmlns' => 'http://firmaelectronica.chiapas.gob.mx/GCD/DoctoGCD',
-            ],
-        ]);
-        //Generacion de cadena unica mediante el ICTI
-        $xmlBase64 = base64_encode($result);
-        $getToken = Tokens_icti::Where('sistema', 'sivyc')->First();
-        if ($getToken) {
-            $response = $this->getCadenaOriginal($xmlBase64, $getToken->token);
-            if ($response->json() == null) {
-                $token = $this->generarToken();
-                $response = $this->getCadenaOriginal($xmlBase64, $token);
-            }
-        } else {// no hay registros
+    //     $result = ArrayToXml::convert($ArrayXml, [
+    //         'rootElementName' => 'DocumentoChis',
+    //         '_attributes' => [
+    //             'version' => '2.0',
+    //             'fecha_creacion' => $dateFormat,
+    //             'no_oficio' => $numOficio,
+    //             'dependencia_origen' => 'Instituto de Capacitación y Vinculación Tecnológica del Estado de Chiapas',
+    //             'asunto_docto' => 'Lista de asistencia LAD-04',
+    //             'tipo_docto' => 'OFC',
+    //             'xmlns' => 'http://firmaelectronica.chiapas.gob.mx/GCD/DoctoGCD',
+    //         ],
+    //     ]);
+    //     //Generacion de cadena unica mediante el ICTI
+    //     $xmlBase64 = base64_encode($result);
+    //     $getToken = Tokens_icti::Where('sistema', 'sivyc')->First();
+    //     if ($getToken) {
+    //         $response = $this->getCadenaOriginal($xmlBase64, $getToken->token);
+    //         if ($response->json() == null) {
+    //             $token = $this->generarToken();
+    //             $response = $this->getCadenaOriginal($xmlBase64, $token);
+    //         }
+    //     } else {// no hay registros
 
-            $token = $this->generarToken();
-            $response = $this->getCadenaOriginal($xmlBase64, $token);
-        }
+    //         $token = $this->generarToken();
+    //         $response = $this->getCadenaOriginal($xmlBase64, $token);
+    //     }
 
-        //Guardado de cadena unica
-        if ($response->json()['cadenaOriginal'] != null) {
-            // $urlFile = $this->uploadFileServer($request->file('doc'), $nameFileOriginal);
-            // $urlFile = $this->uploadFileServer($request->file('doc'), $nameFile);
-            // $datas = explode('*',$urlFile);
+    //     //Guardado de cadena unica
+    //     if ($response->json()['cadenaOriginal'] != null) {
+    //         // $urlFile = $this->uploadFileServer($request->file('doc'), $nameFileOriginal);
+    //         // $urlFile = $this->uploadFileServer($request->file('doc'), $nameFile);
+    //         // $datas = explode('*',$urlFile);
 
-            $dataInsert = DocumentosFirmar::Where('numero_o_clave',$info->clave)->Where('tipo_archivo','Lista de asistencia')->First();
-            if(is_null($dataInsert)) {
-                $dataInsert = new DocumentosFirmar();
-            }
-            $dataInsert->obj_documento = json_encode($ArrayXml);
-            $dataInsert->obj_documento_interno = json_encode($ArrayXml);
-            $dataInsert->status = 'EnFirma';
-            // $dataInsert->link_pdf = $urlFile;
-            $dataInsert->cadena_original = $response->json()['cadenaOriginal'];
-            $dataInsert->tipo_archivo = 'Lista de asistencia';
-            $dataInsert->numero_o_clave = $info->clave;
-            $dataInsert->nombre_archivo = $nameFileOriginal;
-            $dataInsert->documento = $result;
-            $dataInsert->documento_interno = $result;
-            // $dataInsert->md5_file = $md5;
-            $dataInsert->save();
+    //         $dataInsert = DocumentosFirmar::Where('numero_o_clave',$info->clave)->Where('tipo_archivo','Lista de asistencia')->First();
+    //         if(is_null($dataInsert)) {
+    //             $dataInsert = new DocumentosFirmar();
+    //         }
+    //         $dataInsert->obj_documento = json_encode($ArrayXml);
+    //         $dataInsert->obj_documento_interno = json_encode($ArrayXml);
+    //         $dataInsert->status = 'EnFirma';
+    //         // $dataInsert->link_pdf = $urlFile;
+    //         $dataInsert->cadena_original = $response->json()['cadenaOriginal'];
+    //         $dataInsert->tipo_archivo = 'Lista de asistencia';
+    //         $dataInsert->numero_o_clave = $info->clave;
+    //         $dataInsert->nombre_archivo = $nameFileOriginal;
+    //         $dataInsert->documento = $result;
+    //         $dataInsert->documento_interno = $result;
+    //         // $dataInsert->md5_file = $md5;
+    //         $dataInsert->save();
 
-            return redirect()->route('firma.inicio')->with('success', 'Lista de Asistencia Validado Exitosamente!');
-        } else {
-            return redirect()->route('firma.inicio')->with('danger', 'Hubo un Error al Validar. Intente Nuevamente en unos Minutos.');
-        }
-    }
+    //         return redirect()->route('firma.inicio')->with('success', 'Lista de Asistencia Validado Exitosamente!');
+    //     } else {
+    //         return redirect()->route('firma.inicio')->with('danger', 'Hubo un Error al Validar. Intente Nuevamente en unos Minutos.');
+    //     }
+    // }
 
     ### BY JOSE LUIS / VALIDACIÓN DE INCAPACIDAD
     public function valid_incapacidad($dataFirmante){
@@ -676,20 +677,5 @@ class AsistenciaController extends Controller
         // ]);
 
         return $response1;
-    }
-
-    public function update_body() {
-        set_time_limit(0);
-        $asistencias = DocumentosFirmar::Where('tipo_archivo','Lista de asistencia')->Select('id')->orderBy('id','desc')->Get();
-        foreach($asistencias as $dcAsistencia_id) {
-            $asistencia = DocumentosFirmar::Where('id', $dcAsistencia_id->id)->First();
-            $body = $this->create_body($asistencia->numero_o_clave);
-            $array_html['header'] = $body['header'];
-            $array_html['body_html'] = $body['body_html'];
-
-            $asistencia->obj_documento_interno = json_encode($array_html);
-            $asistencia->save();
-        }
-        dd('complete');
     }
 }
