c30b3f8da46b773c62e1b0a63a323bab2bc39b37
Jose-Wicho <luisito08672@gmail.com>
2025-02-05 09:49:48 -0600

    Solucion de obtención de funcionario efirma

--- app/Http/Controllers/efirma/AsistenciaController.php
+++ app/Http/Controllers/efirma/AsistenciaController.php
@@ -339,7 +339,7 @@ class AsistenciaController extends Controller
                         $dataFirmante = DB::Table('tbl_organismos AS org')
                         ->Select('org.id', 'fun.nombre AS funcionario','fun.curp',
                         'fun.cargo','fun.correo', 'org.nombre', 'fun.incapacidad')
-                            ->join('tbl_funcionarios AS fun', 'fun.id','org.id')
+                            ->join('tbl_funcionarios AS fun', 'fun.id_org','org.id')
                             ->where('fun.curp', '=', $puestoUsuario)
                             ->first();
                         if($dataFirmante == null){return "No se encontraron datos del servidor publico";}
