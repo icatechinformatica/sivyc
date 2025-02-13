0f82767ae77fcdaa3c557096764290707bb12444
danielmendez88 <daniboymendez@gmail.com>
2025-02-13 10:55:33 -0600

    modificaciones en qrcode y perfiles laborales

---
+++ resources/views/theme/sivycCredencial/menu.blade.php
@@ -0,0 +1,12 @@
+<!--Navbar -->
+<nav class="mb-1 navbar navbar-expand-lg navbar-dark pink2 darken-4">
+    <a href="https://icatech.gob.mx" class="navbar-brand g-text-underline--hover">
+        <img src="{{asset('img/sivyc.png') }}" alt="SIVyC" height="45">
+    </a>
+    {{-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-555"
+        aria-controls="navbarSupportedContent-555" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent-555">
+    </div> --}}
+</nav>
