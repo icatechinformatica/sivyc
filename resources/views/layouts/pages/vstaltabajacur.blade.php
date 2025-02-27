0e287e9cd4cb2beea9d3867d473490e6985926ac
rpnanguelu@gmail.com <rpnanguelu@gmail.com>
2025-02-27 12:49:09 -0600

    CURSO DISPONIBLE

--- resources/views/layouts/pages/vstaltabajacur.blade.php
+++ resources/views/layouts/pages/vstaltabajacur.blade.php
@@ -1,6 +1,9 @@
 <!-- Creado por Orlando Chávez -->
 @extends('theme.sivyc.layout')
-@section('title', 'Alta/Baja de Instructor | Sivyc Icatech')
+@section('title', 'Disponibilidad del Cruso | Sivyc Icatech')
+@section('content_script_css')
+    <link rel="stylesheet" href="{{asset('css/global.css') }}" />    
+@endsection
 <head>
     <script type="text/javascript">
         function determinador(checkbox)
@@ -12,14 +15,16 @@
     <style>
         .checkbox-xl .custom-control-label::before,
         .checkbox-xl .custom-control-label::after {
-        top: 1.2rem;
-        width: 1.85rem;
-        height: 1.85rem;
+            top: 0rem;
+            width: 2.85rem;
+            height: 2.8rem;
+            background-color:#009887;
+            font-size: 24px;     
         }
 
         .checkbox-xl .custom-control-label {
-        padding-top: 23px;
-        padding-left: 10px;
+            padding-top: 23px;
+            padding-left: 10px
         }
 
         td {
@@ -27,7 +32,8 @@
             vertical-align: middle; /* center checkbox vertically */
         }
         #choice-td{
-            background-color: lightsteelblue;
+            background-color:#009887; 
+            width: 1.85rem;            
         }
         table {
             border: 1px solid;
@@ -38,26 +44,24 @@
         }
     </style>
 </head>
-@section('content')
-    <section class="container g-py-40 g-pt-40 g-pb-0">
+@section('content')        
+    <div class="card-header">
+        Catálogos / Disponibilidad del Curso por Unidad de Capacitación
+    </div>
+    <div class="card card-body">
         <form method="POST" action="{{ route('curso-alta-baja-save') }}" id="alta-bajacurso">
             @csrf
-            <div class="text-center">
-                <h1>Alta/Baja de Curso<h1>
-            </div>
-            <br>
-            <table  id="table-instructor" class="table table-bordered table-responsive-md">
-                <caption>Catalogo de Unidades</caption>
+            <table  id="table-instructor" class="table table-bordered table-responsive-md">                
                 <thead>
                     <tr>
-                        <th scope="col">Unidad</th>
-                        <th scope="col">Disponible</th>
-                        <th scope="col">Unidad</th>
-                        <th scope="col">Disponible</th>
-                        <th scope="col">Unidad</th>
-                        <th scope="col">Disponible</th>
-                        <th scope="col">Unidad</th>
-                        <th scope="col">Disponible</th>
+                        <th scope="col">UNIDAD</th>
+                        <th scope="col">DISPONIBLE</th>
+                        <th scope="col">UNIDAD</th>
+                        <th scope="col">DISPONIBLE</th>
+                        <th scope="col">UNIDAD</th>
+                        <th scope="col">DISPONIBLE</th>
+                        <th scope="col">UNIDAD</th>
+                        <th scope="col">DISPONIBLE</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -499,19 +503,17 @@
                 </tbody>
             </table>
             <br>
-            <div class="row">
-                <div class="col-lg-12 margin-tb">
-                    @can('cursos.update')
-                        <div class="pull-right">
-                            <button type="submit" class="btn btn-success">Confirmar Alta/Baja</button>
-                            <input type="hidden" name="id_available" id='id_available' value="{{$id}}">
-                        </div>
+            <div class="form-row">
+                <div class="d-flex flex-lg-row flex-column col-6 col-lg-3 justify-content-left">            
+                    <a class="btn" href="{{URL::previous()}}"><< Regresar</a>            
+                </div>
+                <div class="d-flex flex-lg-row flex-column col-6 col-lg-9 justify-content-end">                
+                    @can('cursos.update')                        
+                        <button type="submit" class="btn btn-danger">GUARDAR CAMBIOS</button>
+                        <input type="hidden" name="id_available" id='id_available' value="{{$id}}">                        
                     @endcan
-                    <div class="pull-left">
-                        <a class="btn btn-warning" href="{{URL::previous()}}">Regresar</a>
-                    </div>
                 </div>
             </div>
         </form>
-    </section>
+    </div>
 @stop
