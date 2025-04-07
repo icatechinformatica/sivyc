0e7627c7f3997c25ac5e394318e62baad60fe7ed
rpnanguelu@gmail.com <rpnanguelu@gmail.com>
2025-04-07 12:25:31 -0600

    VoBo v2

--- resources/views/solicitudes/vbgrupos/index.blade.php
+++ resources/views/solicitudes/vbgrupos/index.blade.php
@@ -28,26 +28,61 @@
         ?>
         {{ Form::open(['route' => 'solicitudes.vb.grupos', 'method' => 'post', 'id'=>'frm']) }}
             <div class="row form-inline">                  
-                {{ Form::text('clave', $clave ?? '', ['id'=>'clave', 'class' => 'form-control', 'placeholder' => 'GRUPO / CURSO / INSTRUCTOR / UNIDAD', 'aria-label' => 'CLAVE DEL CURSO', 'required' => 'required', 'size' => 80]) }}                
-                @foreach ($estatus as $key => $value)
-                    <div class="form-check">
-                        <input type="radio" class="form-check-input ml-4" name="estatus" id="estatus{{ $key }}" value="{{ $key }}" {{ $key == $status ? 'checked' : '' }}>
-                        <label class="form-check-label" for="estatus{{ $key }}">
-                            {{ $value }}
-                        </label>
-                    </div>
-                @endforeach
-
+                <div class="d-flex flex-lg-row flex-column col-12 col-md-6 col-sm-12 justify-content-left">
+                    {{ Form::text('clave', $clave ?? '', ['id'=>'clave', 'class' => 'form-control', 'placeholder' => 'CURSO / INSTRUCTOR / UNIDAD', 'aria-label' => 'CLAVE DEL CURSO', 'required' => 'required', 'size' => 60]) }}
+                </div>    
+                <div class="d-flex flex-lg-row flex-column col-12 col-md-6 col-sm-12 justify-content-end">
+                    @foreach ($estatus as $key => $value)
+                        <div class="form-check d-flex mt-2">
+                            <input type="radio" class="form-check-input col-md-6" name="estatus" id="estatus{{ $key }}" value="{{ $key }}" {{ $key == $status ? 'checked' : '' }}>
+                            <label class="form-check-label col-md-6 mt-1" for="estatus{{ $key }}">
+                                {{ $value }}
+                            </label>
+                        </div>
+                    @endforeach
+                </div>
             </div>
             <div class="row">
                 @include('solicitudes.vbgrupos.table')
             </div>
+
+
+            {{-- Modal DATOS --}}
+            <div class="modal fade" id="modalDatos" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                {{-- color en el modal --}}
+                <div class="modal-dialog modal-sm modal-notify modal-danger" id="" role="document">
+                <!--Content-->
+                <div class="modal-content text-center">
+                    <!--Header-->
+                    <div class="modal-header d-flex justify-content-center">
+                        {{-- Mensaje para el modal --}}
+                    <p class="heading font-weight-bold">HOLA</p>
+                    </div>
+                    <!--Body-->
+                    <div class="modal-body">
+                        <div class="alert alert-danger alert-dismissible fade show pl-2 text-left" role="alert">
+                           HOLA
+                        </div>
+                        <div class="form-group">
+                            HOLA
+                        </div>
+                    </div>
+                    <!--Footer-->
+                    <div class="modal-footer flex-center">                        
+                        <a type="button" class="btn btn-outline-danger waves-effect" id="" data-dismiss="modal">
+                        <i class="fa fa-times fa-sm text-danger" aria-hidden="true"> </i> &nbsp; CERRAR</a>
+                    </div>
+                </div>
+                <!--/.Content-->
+                </div>
+            </div>
+            {{-- FIN Modal DATOS --}}
+
         {!! Form::close() !!}
     </div>
     @section('script_content_js')
         <script language="javascript">    
-         $(function(){
-            //metodo
+        $(function(){            
             $.ajaxSetup({
                 headers: {
                     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
@@ -55,21 +90,22 @@
             });
         });
         function cambia_estado(id, status){
+            estado = status.prop('checked');
             $.ajax({
                     method: "POST",
                     url: "vbgrupos/vistobueno",
                     data: {
                         id: id,
-                        estado: status
+                        estado: estado
                     }
             })
             .done(function( msg ) { alert(msg); });
         }
 
-        function actualizadata(){
+        function actualiza_data(){
             var clave = $('#clave').val();
-            var estatus = $('#estatus').val();
-            if (clave.length >= 3 || clave.length ==0){ //alert(estatus);
+            var estatus = $('input[name="estatus"]:checked').val();
+            if (clave.length >= 3 || clave.length ==0 || estatus.length>0){
                 $.ajax({
                     url: "vbgrupos/buscar",
                     method: 'POST',
@@ -85,11 +121,14 @@
         }
 
         $(document).ready(function(){
-            $('#clave').on('keyup', function() { actualizadata(); });            
-            $('input[name="estatus"]').on('click', function() {  alert("pasa");
-                actualizadata();
+            $('#clave').on('keyup', function() { actualiza_data(); });            
+            $('input[name="estatus"]').on('click', function() { 
+                actualiza_data();
             });
         });
+        
+        //$('#modalDatos').modal('show');
+        
         </script>
     @endsection
 @endsection
