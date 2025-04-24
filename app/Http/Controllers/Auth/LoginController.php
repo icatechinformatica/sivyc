8d1c52988625328156474d2c7a2600096a52e4ee
danielmendez88 <daniboymendez@gmail.com>
2025-04-24 10:06:40 -0600

    modificacion en metodo authenticated en LoginController quitar dd agregar log 24042025

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -50,7 +50,7 @@ class LoginController extends Controller
     {
         //solicitudes.vb.grupos
         if ($user->id === 1) {
-            dd($user);
+            \Log::info($user);
             return redirect()->route('solicitudes.vb.grupos'); // Ruta única para el usuario 1
         }
 
