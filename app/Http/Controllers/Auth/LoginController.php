1096cbead79396bfe2f041f40db46419dbd6633d
danielmendez88 <daniboymendez@gmail.com>
2025-04-24 14:41:09 -0600

    cambios en loginController 24042025

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -58,32 +58,4 @@ class LoginController extends Controller
         // Comportamiento normal para otros usuarios
         return redirect()->intended($this->redirectPath());
     }
-
-    public function login(Request $request)
-    {
-        $this->validateLogin($request); // usa las reglas del trait
-
-        // Si el usuario ha superado el límite de intentos
-        if (method_exists($this, 'hasTooManyLoginAttempts') &&
-            $this->hasTooManyLoginAttempts($request)) {
-            $this->fireLockoutEvent($request);
-            return $this->sendLockoutResponse($request);
-        }
-
-        $credentials = $request->only('email', 'password');
-
-        if (!Auth::attempt($credentials, $request->filled('remember'))) {
-            \Log::warning('Login fallido', [
-                'email' => $credentials['email'],
-                'ip' => $request->ip(),
-                'hora' => now()->toDateTimeString(),
-            ]);
-
-            $this->incrementLoginAttempts($request); // incrementa intentos fallidos
-            return $this->sendFailedLoginResponse($request);
-        }
-
-        // Autenticación exitosa
-        return $this->sendLoginResponse($request);
-    }
 }
