136fd59bb725f90219b50072f52a8e8523efbfa9
Jose-Wicho <luisito08672@gmail.com>
2025-05-07 21:36:36 -0600

    agregar funcion de cambio de instructor en visto bueno

--- resources/views/solicitudes/vbgrupos/table.blade.php
+++ resources/views/solicitudes/vbgrupos/table.blade.php
@@ -10,11 +10,15 @@
                 <th scope="col" width="90px">INICIO</th>
                 <th scope="col" width="90px">TERMINO</th>
                 <th scope="col" width="13%">UNIDAD/AM</th>
-
+                <th scope="col" width="0%">RECHAZAR</th>
             </tr>
         </thead>
         <tbody id="result_table">
             @foreach ($data as $item)
+                @php
+                    if(strlen($item->curso) >= 18) $curso = mb_substr($item->curso, 0, 18, 'UTF-8')."..";
+                    else $curso = $item->curso;
+                @endphp
                 <tr>
                     <td class="text-center">
                         <div class="form-check">
@@ -35,13 +39,22 @@
                     <td>{{ date('d/m/Y', strtotime($item->inicio)) }}</td>
                     <td>{{ date('d/m/Y', strtotime($item->termino)) }}</td>
                     <td>{{ $item->unidad }}</td>
-                </tr>
-            @endforeach
-                <tr>
-                    <td colspan="11" >
-                       {{ $data->render() }}
+                    <td class="text-center">
+                        @if($item->clave == '0')
+                        <a onclick="modal_motivo('{{ $curso }}','{{ $item->id }}')">
+                            <i class="fas fa-window-close fa-2x fa-danger"></i>
+                        </a>
+                        @else
+                            {{ $item->turnado}}
+                        @endif
                     </td>
                 </tr>
+            @endforeach
+            <tr>
+                <td colspan="11" >
+                    {{$data->render()}}
+                </td>
+            </tr>
         </tbody>
         <tfoot>
         </tfoot>
