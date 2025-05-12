c9b9081848e74098273dad8ff35d3d30f429b101
rpnanguelu@gmail.com <rpnanguelu@gmail.com>
2025-05-12 08:39:46 -0600

    VER GRUPO VoBo

--- resources/views/preinscripcion/buscar/table.blade.php
+++ resources/views/preinscripcion/buscar/table.blade.php
@@ -9,7 +9,7 @@
                 <th scope="col">UNIDAD</th>    
                 <th scope="col">TURNADO A</th>
                 <th scope="col" class="text-center">OPCIONES</th>
-                @can('preinscripcion.grupovobo')
+                @can('enviar.grupo.vobo')
                     <th scope="col" class="text-center">VoBo</th>
                 @endcan
             </tr>
@@ -31,14 +31,13 @@
                         @endif
                     </td>
                     @can('preinscripcion.grupovobo')
-                        <td class="text-center">
-                            @if(!$item->id_instructor)  
-                                @if($item->turnado == 'VINCULACION')                       
-                                    <a class="nav-link" ><i class="fa fa-edit  fa-2x fa-lg text-success" title="Editar" onclick="show2('{{$item->folio_grupo}}')"></i></a>
-                                @else 
-                                    <a class="nav-link" ><i class="fa fa-search  fa-2x fa-lg text-info" title="Ver" onclick="show2('{{$item->folio_grupo}}')"></i></a>
-                                @endif
+                        <td class="text-center">                            
+                            @if($item->turnado == 'VINCULACION')                       
+                                <a class="nav-link" ><i class="fa fa-edit  fa-2x fa-lg text-success" title="Editar" onclick="show2('{{$item->folio_grupo}}')"></i></a>
+                            @else 
+                                <a class="nav-link" ><i class="fa fa-search  fa-2x fa-lg text-info" title="Ver" onclick="show2('{{$item->folio_grupo}}')"></i></a>
                             @endif
+                            
                         </td>
                     @endcan
                 </tr>
