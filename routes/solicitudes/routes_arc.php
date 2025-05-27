b14016a9ee496ebdbf70ae504c679812d79ac040
rpnanguelu@gmail.com <rpnanguelu@gmail.com>
2025-05-27 11:57:14 -0600

    RUTA GUARDAR FECHA ARC01

--- routes/solicitudes/routes_arc.php
+++ routes/solicitudes/routes_arc.php
@@ -1,6 +1,7 @@
 <?php
 use Illuminate\Support\Facades\Route;
 Route::middleware(['auth'])->group(function(){            
-    Route::post('/solicitudes/aperturas/soporte_pago', 'Solicitudes\aperturasController@soporte_pago')->name('solicitudes.aperturas.soporte_pago');
+    Route::post('/solicitudes/aperturas/soporte_pago', 'Solicitudes\aperturasController@soporte_pago')->name('solicitudes.aperturas.soporte_pago')->middleware('can:solicitudes.aperturas');
+    Route::post('/solicitudes/aperturas/guardar_fecha', 'Solicitudes\aperturasController@guardar_fecha')->name('solicitudes.aperturas.guardar_fecha')->middleware('can:solicitudes.aperturas');
   
 });
