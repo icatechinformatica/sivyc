feb8b478ed37e43450d9df3d0063876a1299d979
Ricardo Sauceda <christopherrgs2@hotmail.com>
2025-06-30 11:53:08 -0600

    Feat: Actualizar referencia de la dependencia permiso-rol-menu y ajustar tiempos en composer.lock Refactor: Eliminar proveedores no utilizados en app.php

--- config/app.php
+++ config/app.php
@@ -146,7 +146,6 @@ return [
         Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
         Illuminate\Cookie\CookieServiceProvider::class,
         Illuminate\Database\DatabaseServiceProvider::class,
-        Spatie\Permission\PermissionServiceProvider::class,
         Illuminate\Encryption\EncryptionServiceProvider::class,
         Illuminate\Filesystem\FilesystemServiceProvider::class,
         Illuminate\Foundation\Providers\FoundationServiceProvider::class,
@@ -181,7 +180,6 @@ return [
         Laravel\Passport\PassportServiceProvider::class,
         App\Providers\ReporteRf001Provider::class,
         App\Providers\DocumentacionPagoInstructorProvider::class,
-        App\Providers\StorageServiceProvider::class,
         App\Providers\CredencialProvider::class,
     ],
 
