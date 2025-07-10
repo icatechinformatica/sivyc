50c4e150ddb9356529e9ca08cd9bc18e2adc85ee
Ricardo Sauceda <christopherrgs2@hotmail.com>
2025-07-10 09:18:39 -0600

    Feat: implementar servicios para gestionar funcionarios y obtener funcionarios sin usuario asociado

--- app/Interfaces/FuncionariosRepositoryInterface.php
+++ app/Interfaces/FuncionariosRepositoryInterface.php
@@ -5,5 +5,6 @@ use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
 interface FuncionariosRepositoryInterface {
     public function all(): LengthAwarePaginator;
+    public function getWithOutUser(): LengthAwarePaginator;
     public function createUser(array $data): array; // Define the return type for createUser method
 }
