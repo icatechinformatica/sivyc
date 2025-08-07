33a9ac5a7a1641143a82bd0d058a78350ff1c81b
Ricardo Sauceda <christopherrgs2@hotmail.com>
2025-08-07 10:00:52 -0600

    Feat: Hacer campos de relaciones en tbl_grupos opcionales y permitir eliminación en cascada

--- database/migrations/2025_08_06_121045_create_tbl_grupos_table.php
+++ database/migrations/2025_08_06_121045_create_tbl_grupos_table.php
@@ -16,27 +16,27 @@ return new class extends Migration
         Schema::create('tbl_grupos', function (Blueprint $table) {
             $table->id();
 
-            $table->foreignId('id_unidad')->constrained('tbl_unidades');
-            $table->foreignId('id_instructor')->constrained('tbl_instructores');
+            $table->foreignId('id_unidad')->nullable()->constrained('tbl_unidades')->onDelete('set null');
+            $table->foreignId('id_instructor')->nullable()->constrained('tbl_instructores')->onDelete('set null');
 
-            $table->string('clave_grupo', 10)->unique();
-            $table->foreignId('id_curso')->constrained('cursos')->onDelete('set null');
+            $table->string('clave_grupo', 10)->nullable()->unique();
+            $table->foreignId('id_curso')->nullable()->constrained('cursos')->onDelete('set null');
 
-            $table->foreignId('id_modalidad')->constrained('tbl_aux_modalidades')->onDelete('set null'); // * CAE y EXT
-            $table->foreignId('id_imparticion')->constrained('tbl_aux_imparticiones')->onDelete('set null'); // * Presencial y A Distancia
-            $table->foreignId('id_servicio')->constrained('tbl_aux_servicios')->onDelete('set null'); // * Curso y Certificación
+            $table->foreignId('id_modalidad')->nullable()->constrained('tbl_aux_modalidades')->onDelete('set null'); // * CAE y EXT
+            $table->foreignId('id_imparticion')->nullable()->constrained('tbl_aux_imparticiones')->onDelete('set null'); // * Presencial y A Distancia
+            $table->foreignId('id_servicio')->nullable()->constrained('tbl_aux_servicios')->onDelete('set null'); // * Curso y Certificación
 
-            $table->foreignId('id_organismo_publico')->constrained('organismos_publicos')->onDelete('set null');
-            $table->foreignId('id_municipio')->constrained('tbl_municipios')->onDelete('set null');
-            $table->foreignId('id_localidad')->constrained('tbl_localidades')->onDelete('set null');
+            $table->foreignId('id_organismo_publico')->nullable()->constrained('organismos_publicos')->onDelete('set null');
+            $table->foreignId('id_municipio')->nullable()->constrained('tbl_municipios')->onDelete('set null');
+            $table->foreignId('id_localidad')->nullable()->constrained('tbl_localidades')->onDelete('set null');
             $table->string('programa')->nullable();
 
             $table->string('efisico', 255)->nullable();
             $table->string('cespecifico', 60)->nullable();
-            $table->foreignId('id_tipo_exoneracion')->constrained('tbl_exoneraciones')->onDelete('set null');
+            $table->foreignId('id_tipo_exoneracion')->nullable()->constrained('tbl_exoneraciones')->onDelete('set null');
             $table->string('medio_virtual', 25)->nullable();
             $table->string('link_virtual', 255)->nullable();
-            $table->foreignId('id_cerss')->constrained('cerss')->onDelete('set null');
+            $table->foreignId('id_cerss')->nullable()->constrained('cerss')->onDelete('set null');
             $table->boolean('asis_finalizado')->default(false);
             $table->boolean('calif_finalizado')->default(false);
             $table->string('num_revision', 255)->nullable();
