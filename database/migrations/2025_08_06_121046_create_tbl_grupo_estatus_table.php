d855ff3ff9aa4efa295a1f976a57cb1669bd3264
Ricardo Sauceda <christopherrgs2@hotmail.com>
2025-08-08 09:58:50 -0600

    feat(grupos): Modificar la migración de tbl_grupo_estatus para permitir eliminación en cascada y agregar nuevos campos

--- database/migrations/2025_08_06_121046_create_tbl_grupo_estatus_table.php
+++ database/migrations/2025_08_06_121046_create_tbl_grupo_estatus_table.php
@@ -16,8 +16,13 @@ return new class extends Migration
         Schema::create('tbl_grupo_estatus', function (Blueprint $table) {
             $table->id();
             $table->foreignId('id_grupo')->constrained('tbl_grupos')->onDelete('cascade');
-            $table->foreignId('id_estatus')->constrained('tbl_aux_estatus')->onDelete('cascade');
-            $table->timestamps();
+            $table->foreignId('id_estatus')->constrained('tbl_aux_estatus')->onDelete('set null');
+            $table->foreignId('id_usuario')->constrained('tblz_usuarios')->onDelete('set null');
+            $table->string('observaciones')->nullable();
+            $table->string('memorandum')->nullable();
+            $table->string('ruta_documento')->nullable();
+            $table->date('fecha_cambio')->nullable();
+            $table->boolean('es_ultimo_estatus')->default(false);
         });
     }
 
