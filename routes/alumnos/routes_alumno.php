e12e9df7bc24a7614826fb6aa00bd521ef16b203
Ricardo Sauceda <christopherrgs2@hotmail.com>
2025-08-19 09:54:22 -0600

    feat(grupos): agregar lógica para generar folio único al crear grupos y definir rutas para gestión de grupos y alumnos

---
+++ routes/alumnos/routes_alumno.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Alumno\AlumnoController;
+
+Route::group(['middleware' => ['auth'], 'prefix' => 'alumnos'], function () {
+    Route::get('/cosulta', [AlumnoController::class, 'index'])->name('alumnos.consulta.alumno');
+    Route::post('/guardar', [AlumnoController::class, 'store'])->name('alumnos.store');
+    Route::post('/consultar_curp', [AlumnoController::class, 'consultarCurp'])->name('alumnos.consultar.curp');
+    Route::post('/obtener/datos/curp/{encodeCURP}', [AlumnoController::class, 'obtenerDatosCurp'])->name('alumnos.obtener.datos.curp');
+
+    Route::get('/registro/alumno/{encodeCURP}', [AlumnoController::class, 'verRegistroAlumno'])->name('alumnos.ver.registro.alumno');
+    Route::get('/registro/nuevo/alumno/{encodeCURP}', [AlumnoController::class, 'nuevoRegistroAlumno'])->name('alumnos.nuevo.registro.alumno');
+
+    Route::post('/guardar/seccion/alumno', [AlumnoController::class, 'guardarSeccionAlumno'])->name('alumnos.guardar.seccion.alumno');
+});
