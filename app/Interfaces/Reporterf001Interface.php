9a5915d1c2a59edc65dd2574bceb97c15cacc402
DANIEL MENDEZ <daniboymendez@gmail.com>
2025-09-03 14:37:11 -0600

    modificaciones a buzon rf001 y filtrado, asi como mejoras en la cancelacion 03092025

--- app/Interfaces/Reporterf001Interface.php
+++ app/Interfaces/Reporterf001Interface.php
@@ -4,7 +4,7 @@ namespace App\Interfaces;
 interface Reporterf001Interface {
     public function index($user);
     public function getReciboQry($unidad);
-    public function sentRF001Format($unidad);
+    public function sentRF001Format($unidad, $memorandum);
     public function getDetailRF001Format($concentrado);
     public function storeData(array $request);
     public function updateFormatoRf001($request, $id);
