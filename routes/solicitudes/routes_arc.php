43aa0a1c8ea724bad61aeea592586449b4a8e310
rpnanguelu@gmail.com <rpnanguelu@gmail.com>
2025-11-20 09:59:11 -0600

    FECHA EXO DTA

--- routes/solicitudes/routes_arc.php
+++ routes/solicitudes/routes_arc.php
@@ -5,4 +5,5 @@ Route::middleware(['auth'])->group(function(){
     Route::post('/solicitudes/aperturas/guardar_fecha', 'Solicitudes\aperturasController@guardar_fecha')->name('solicitudes.aperturas.guardar_fecha')->middleware('can:solicitudes.aperturas');
     Route::post('/solicitudes/aperturas/pdfagenda', 'Solicitudes\aperturasController@pdfAgendaAnexo')->name('solicitudes.generar.agenda.anexo')->middleware('can:solicitudes.aperturas');
     Route::get('/solicitudes/aperturas/pdfagenda', 'Solicitudes\aperturasController@pdfAgendaanexo')->name('solicitudes.generar.agenda.anexo')->middleware('can:solicitudes.aperturas');
+    Route::post('/solicitudes/exoneraciones/guardar_fecha', 'Solicitudes\exoneracionesController@guardar_fecha')->name('solicitudes.exoneraciones.guardar_fecha')->middleware('can:solicitudes.exoneracion');
 });
